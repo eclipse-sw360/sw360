eclipse-sw360/sw360

commit df54014729a05e977095c187f65ce7f0d22f433f

chore(cleanup): drop unused and outdated code reated to the codescoop integration
Signed-off-by: Maximilian Huber <maximilian.huber@tngtech.com>

frontend/sw360-portlet/src/main/resources/META-INF/resources/html/components/view.jsp
@@ -64,11 +64,7 @@
     <portlet:param name="<%=PortalConstants.LICENSE_ID%>" value="<%=PortalConstants.FRIENDLY_URL_PLACEHOLDER_ID%>"/>
 </liferay-portlet:renderURL>
 
-<portlet:resourceURL var="sw360CompositeUrl">
-    <portlet:param name="<%=PortalConstants.ACTION%>" value='<%=PortalConstants.CODESCOOP_ACTION_COMPOSITE%>'/>
-</portlet:resourceURL>
-
-<div class="container" style="display: none;">
+div class="container" style="display: none;">
 	<div class="row">
 		<div class="col-3 sidebar">
 			<div class="card-deck">
@@ -170,31 +166,6 @@
 <%--for javascript library loading --%>
 <%@ include file="/html/utils/includes/requirejs.jspf" %>
 <script>
-    var renderCallback = function () {
-    };
-    var dataGetter = function(field) {
-    };
-</script>
-
-<core_rt:if test="${codescoopActive}">
-    <script>
-        window.codescoopEnabled = true;
-        document.addEventListener("DOMContentLoaded", function() {
-            require(['modules/codeScoop' ], function(codeScoop) {
-                var api = new codeScoop();
-                api.activateIndexes("componentsTable", "<%=sw360ComponentsURL%>", "<%=sw360CompositeUrl%>");
-                renderCallback = api._update_indexes;
-                dataGetter = api._get_composite_data_item;
-            });
-            document
-                .getElementById("componentsTable")
-                .getElementsByTagName("tfoot")[0]
-                .getElementsByTagName("th")[0]
-                .setAttribute("colspan", 10)
-        });
-    </script>
-</core_rt:if>
-<script>
     AUI().use('liferay-portlet-url', function () {
         var PortletURL = Liferay.PortletURL;
 
@@ -246,42 +217,6 @@
             // create and render data table
             function createComponentsTable() {
                 var columnDefs =  [];
-                var columns = [
-                    {"title": "Vendor", data: "vndrs"},
-                    {"title": "Component Name", data: "name", render: {display: renderComponentNameLink}},
-                    {"title": "Main Licenses", data: "lics", render: {display: renderLicenseLink}},
-                    {"title": "Component Type", data: "cType"},
-                    {"title": "Actions", data: "id", render: {display: renderComponentActions}, className: 'two actions', orderable: false }
-                ];
-                var printColumns = [0, 1, 2, 3];
-
-                if (window.codescoopEnabled) {
-                    columns = [
-                        {"title": "Logo", data: function(row, type, val, meta){
-                                return dataGetter(row.DT_RowId, 'logo');
-                            }},
-                        {"title": "Vendor", data: "vndrs"},
-                        {"title": "Component Name", data: "name", render: {display: renderComponentNameLink}},
-                        {"title": "Main Licenses", data: "lics", render: {display: renderLicenseLink}},
-                        {"title": "Rate", data: function(row, type, val, meta){
-                                return dataGetter(row.DT_RowId, 'rate');
-                            }},
-                        {"title": "Interest", data: function(row, type, val, meta){
-                                return dataGetter(row.DT_RowId, 'index', 'interest');
-                            }},
-                        {"title": "Activity", data: function(row, type, val, meta){
-                                return dataGetter(row.DT_RowId, 'index', 'activity');
-                            }},
-                        {"title": "Health", data: function(row, type, val, meta){
-                                return dataGetter(row.DT_RowId, 'index', 'health');
-                            }},
-                        {"title": "Component Type", data: "cType"},
-                        {"title": "Actions", data: "id", render: {display: renderComponentActions}, className: 'two actions', orderable: false }
-                    ];
-                    columnDefs = [{ "orderable": false, "targets": [0, 4, 5, 6 ,7] }];
-                    printColumns = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-                }
-
                 var componentsTable = datatables.create('#componentsTable', {
                     bServerSide: true,
                     sAjaxSource: '<%=sw360ComponentsURL%>',

backend/src/pom.xml
@@ -42,21 +42,6 @@
         <module>src-wsimport</module>
     </modules>
 
-    <profiles>
-        <profile>
-            <id>codescoop</id>
-            <modules>
-                <module>src-codescoop</module>
-            </modules>
-        </profile>
-        <profile>
-            <id>default</id>
-            <activation>
-                <activeByDefault>true</activeByDefault>
-            </activation>
-        </profile>
-    </profiles>
-
     <dependencies>
         <dependency>
             <groupId>org.eclipse.sw360</groupId>

backend/src/src-codescoop/pom.xml
@@ -1,53 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
-  ~ Copyright Siemens AG, 2013-2015. Part of the SW360 Portal Project.
-  ~
-  ~ SPDX-License-Identifier: EPL-1.0
-  ~
-  ~ All rights reserved. This program and the accompanying materials
-  ~ are made available under the terms of the Eclipse Public License v1.0
-  ~ which accompanies this distribution, and is available at
-  ~ http://www.eclipse.org/legal/epl-v10.html
-  -->
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-    <modelVersion>4.0.0</modelVersion>
-    <dependencies>
-        <dependency>
-            <groupId>com.codescoop</groupId>
-            <artifactId>osman</artifactId>
-            <version>0.4.5</version>
-            <scope>compile</scope>
-        </dependency>
-        <dependency>
-            <groupId>org.springframework</groupId>
-            <artifactId>spring-beans</artifactId>
-            <version>${spring.version}</version>
-        </dependency>
-        <dependency>
-            <groupId>org.springframework</groupId>
-            <artifactId>spring-context</artifactId>
-            <version>${spring.version}</version>
-        </dependency>
-        <dependency>
-            <groupId>org.springframework</groupId>
-            <artifactId>spring-webmvc</artifactId>
-            <version>${spring.version}</version>
-        </dependency>
-        <dependency>
-            <groupId>javax.portlet</groupId>
-            <artifactId>portlet-api</artifactId>
-            <version>2.0</version>
-            <scope>compile</scope>
-        </dependency>
-    </dependencies>
-    <parent>
-        <groupId>org.eclipse.sw360</groupId>
-        <artifactId>backend-src</artifactId>
-        <version>${revision}</version>
-    </parent>
-
-    <artifactId>src-codescoop</artifactId>
-    <packaging>jar</packaging>
-    <name>src-codescoop</name>
-</project>

backend/src/src-codescoop/src/main/java/org/eclipse/sw360/codescoop/CodescoopConfig.java
@@ -1,19 +1,0 @@
-/*
- * Copyright Siemens AG, 2015. Part of the SW360 Portal Project.
- *
- * SPDX-License-Identifier: EPL-1.0
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
-package org.eclipse.sw360.codescoop;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"org.eclipse.sw360.codescoop"})
-public class CodescoopConfig {
-}

backend/src/src-codescoop/src/main/java/org/eclipse/sw360/codescoop/CodescoopHandler.java
@@ -1,236 +1,0 @@
-/*
- * Copyright Siemens AG, 2013-2017. Part of the SW360 Portal Project.
- *
- * SPDX-License-Identifier: EPL-1.0
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
-package org.eclipse.sw360.codescoop;
-
-import com.codescoop.client.ClientException;
-import com.codescoop.client.ClosableCodescoopClient;
-import com.codescoop.client.CodescoopClient;
-import com.codescoop.client.model.autocomplete.AutocompleteResponse;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.log4j.Logger;
-import org.apache.thrift.TException;
-import org.eclipse.sw360.datahandler.common.CommonUtils;
-import org.eclipse.sw360.datahandler.thrift.codescoop.*;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Properties;
-
-import static org.apache.commons.lang.StringUtils.isNotBlank;
-
-/**
- * Implementation of the Thrift service
- *
- * @author alex.skorohod@codescoop.com
- */
-@Component
-public class CodescoopHandler implements CodescoopService.Iface {
-
-    private static final Logger log = Logger.getLogger(CodescoopHandler.class);
-
-    private static final String PROPERTY_FILE = "/codescoop.properties";
-    private static final String PROPERTY_HOST = "codescoop.host";
-    private static final String PROPERTY_CLIENT = "codescoop.client";
-    private static final String PROPERTY_CREDENTIALS = "codescoop.credentials";
-    private static final String PROPERTY_PROXY = "codescoop.proxy";
-    private static final ObjectMapper mapper = new ObjectMapper();
-    private CodescoopClient client;
-
-    public CodescoopHandler() {
-        Properties props = CommonUtils.loadProperties(CodescoopHandler.class, PROPERTY_FILE);
-        String host = props.getProperty(PROPERTY_HOST, null);
-        String client = props.getProperty(PROPERTY_CLIENT, null);
-        String credentials = props.getProperty(PROPERTY_CREDENTIALS, null);
-        String proxy = props.getProperty(PROPERTY_PROXY, null);
-
-        log.info("Codescoop client construct : host=" + host + ", client=" + client
-                + ", credentials=" + credentials + ", proxy=" + proxy);
-
-        try {
-            if (isNotBlank(host) && isNotBlank(client) && isNotBlank(credentials)) {
-                this.client = ClosableCodescoopClient
-                        .build()
-                        .withApiUrl(host)
-                        .withApiUser(client)
-                        .withApiKey(credentials)
-                        .withProxyHost(proxy)
-                        .create();
-            }
-        } catch (Exception e) {
-            log.warn("Codescoop init client error", e);
-        } finally {
-            log.warn(client == null ? "Codescoop integration disabled" : "Codescoop client was established");
-        }
-    }
-
-    @Override
-    public boolean isEnabled() throws TException {
-        return this.client != null;
-    }
-
-    @Override
-    public List<CodescoopComponent> searchComponents(CodescoopComponentSearch request) throws TException {
-        try {
-            List<com.codescoop.client.model.component.Component> response =
-                    this.client.searchComponents(ModelTranslator.translateSearchRequest(request));
-            return ModelTranslator.translateComponent(response);
-        } catch (ClientException e) {
-            log.error("Error search components", e);
-            throw new TException("search codescoop component error", e);
-        }
-    }
-
-    @Override
-    public String proceedComponentsJson(String json) throws TException {
-        CodescoopComponentSearch request;
-        try {
-            request = mapper.readValue(json, CodescoopComponentSearch.class);
-        } catch (Exception e) {
-            log.error("Error parse json", e);
-            throw new TException("Error parse json", e);
-        }
-
-        try {
-            List<CodescoopComponent> response = this.searchComponents(request);
-            return mapper.writeValueAsString(response);
-        } catch (Exception e) {
-            log.error("Error codescoop response", e);
-            throw new TException("Error parse codescoop response", e);
-        }
-    }
-
-    @Override
-    public List<CodescoopComponent> searchComponentsComposite(List<CodescoopComponentSearch> requestList)
-            throws TException {
-        try {
-            List<com.codescoop.client.model.component.Component> response =
-                    this.client.searchComponentsComposite(ModelTranslator.translateSearchRequest(requestList));
-            return ModelTranslator.translateComponent(response);
-        } catch (ClientException e) {
-            log.error("Error search composite", e);
-            throw new TException("fetch codescoop composite error");
-        }
-    }
-
-    @Override
-    public String proceedComponentsCompositeJson(String json) throws TException {
-        List<CodescoopComponentSearch> requestList;
-        try {
-            requestList = mapper.readValue(json, new TypeReference<List<CodescoopComponentSearch>>() {
-            });
-        } catch (Exception e) {
-            log.error("Error parse json", e);
-            throw new TException("Error parse json", e);
-        }
-
-        try {
-            List<CodescoopComponent> codescoopData = this.searchComponentsComposite(requestList);
-            return mapper.writeValueAsString(codescoopData);
-        } catch (Exception e) {
-            log.error("Error parse codescoop response", e);
-            throw new TException("Error parse codescoop response", e);
-        }
-    }
-
-    @Override
-    public List<CodescoopComponent> searchComponentsPurl(List<String> purlList) throws TException {
-        try {
-            List<com.codescoop.client.model.component.Component> response = this.client.searchComponentsPurl(purlList);
-            return ModelTranslator.translateComponent(response);
-        } catch (ClientException e) {
-            log.error("Error search purl", e);
-            throw new TException("fetch codescoop purl error");
-        }
-    }
-
-    @Override
-    public String proceedComponentsPurlJson(String json) throws TException {
-        List<String> purlList;
-        try {
-            purlList = mapper.readValue(json, new TypeReference<List<String>>() {
-            });
-        } catch (Exception e) {
-            log.error("Error parse json", e);
-            throw new TException("Error parse json", e);
-        }
-
-        try {
-            List<CodescoopComponent> codescoopData = this.searchComponentsPurl(purlList);
-            return mapper.writeValueAsString(codescoopData);
-        } catch (Exception e) {
-            log.error("Error parse codescoop response", e);
-            throw new TException("Error parse codescoop response", e);
-        }
-    }
-
-    @Override
-    public List<CodescoopRelease> searchComponentReleases(CodescoopComponentSearch request) throws TException {
-        try {
-            List<com.codescoop.client.model.component.Release> response =
-                    this.client.searchComponentReleases(ModelTranslator.translateSearchRequest(request));
-            return ModelTranslator.translateRelease(response);
-        } catch (ClientException e) {
-            log.error("Error search components", e);
-            throw new TException("fetch codescoop component error");
-        }
-    }
-
-    @Override
-    public String proceedComponentReleasesJson(String json) throws TException {
-        CodescoopComponentSearch request;
-        try {
-            request = mapper.readValue(json, CodescoopComponentSearch.class);
-        } catch (Exception e) {
-            log.error("Error parse json", e);
-            throw new TException("Error parse json", e);
-        }
-
-        try {
-            List<CodescoopRelease> codescoopData = this.searchComponentReleases(request);
-            return mapper.writeValueAsString(codescoopData);
-        } catch (Exception e) {
-            log.error("Error parse codescoop response", e);
-            throw new TException("Error parse codescoop response", e);
-        }
-    }
-
-    @Override
-    public CodescoopAutocompleteResponse autocomplete(CodescoopAutocompleteRequest request) throws TException {
-        try {
-            AutocompleteResponse response =
-                    this.client.autocomplete(ModelTranslator.translateAutocompleteRequest(request));
-            return ModelTranslator.translateAutocompleteResponse(response);
-        } catch (ClientException e) {
-            log.error("Error autocomplete codescoop", e);
-            throw new TException("autocomplete codescoop error", e);
-        }
-    }
-
-    @Override
-    public String proceedAutocompleteJson(String json) throws TException {
-        CodescoopAutocompleteRequest request;
-        try {
-            request = mapper.readValue(json, CodescoopAutocompleteRequest.class);
-        } catch (Exception e) {
-            log.error("Error parse json", e);
-            throw new TException("Error parse json", e);
-        }
-
-        try {
-            CodescoopAutocompleteResponse codescoopData = this.autocomplete(request);
-            return mapper.writeValueAsString(codescoopData);
-        } catch (Exception e) {
-            log.error("Error parse codescoop response", e);
-            throw new TException("Error parse codescoop response", e);
-        }
-    }
-}

backend/src/src-codescoop/src/main/java/org/eclipse/sw360/codescoop/CodescoopServlet.java
@@ -1,30 +1,0 @@
-/*
- * Copyright Siemens AG, 2013-2017. Part of the SW360 Portal Project.
- *
- * SPDX-License-Identifier: EPL-1.0
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
-package org.eclipse.sw360.codescoop;
-
-import org.apache.thrift.protocol.TCompactProtocol;
-import org.eclipse.sw360.datahandler.thrift.codescoop.CodescoopService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
-/**
- * Codescoop Servlet instantiation
- *
- * @author alex.skorohod@codescoop.com
- */
-@Controller
-public class CodescoopServlet extends CodescoopSpringServlet {
-
-    @Autowired
-    public CodescoopServlet(CodescoopHandler handler) {
-        super(new CodescoopService.Processor<>(handler), new TCompactProtocol.Factory());
-    }
-}

backend/src/src-codescoop/src/main/java/org/eclipse/sw360/codescoop/CodescoopSpringServlet.java
@@ -1,42 +1,0 @@
-/*
- * Copyright Siemens AG, 2013-2015. Part of the SW360 Portal Project.
- *
- * SPDX-License-Identifier: EPL-1.0
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
-package org.eclipse.sw360.codescoop;
-
-import org.apache.thrift.TProcessor;
-import org.apache.thrift.protocol.TProtocolFactory;
-import org.eclipse.sw360.projects.Sw360ThriftServlet;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@RequestMapping("/thrift")
-public class CodescoopSpringServlet extends Sw360ThriftServlet {
-
-    public CodescoopSpringServlet(TProcessor processor, TProtocolFactory protocolFactory) {
-        super(processor, protocolFactory);
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doPost(request, response);
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doGet(request, response);
-    }
-}

backend/src/src-codescoop/src/main/java/org/eclipse/sw360/codescoop/ModelTranslator.java
@@ -1,162 +1,0 @@
-/*
- * Copyright Siemens AG, 2013-2017. Part of the SW360 Portal Project.
- *
- * SPDX-License-Identifier: EPL-1.0
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
-package org.eclipse.sw360.codescoop;
-
-import com.codescoop.client.model.autocomplete.AutocompleteRequest;
-import com.codescoop.client.model.autocomplete.AutocompleteResponse;
-import com.codescoop.client.model.component.*;
-import com.codescoop.client.model.search.SearchRequest;
-import org.eclipse.sw360.datahandler.thrift.codescoop.*;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import static org.apache.commons.lang.StringUtils.isNotBlank;
-import static org.apache.commons.lang.StringUtils.mid;
-
-public class ModelTranslator {
-
-    public static List<SearchRequest> translateSearchRequest(List<CodescoopComponentSearch> sw360Request) {
-        return sw360Request.stream().map(ModelTranslator::translateSearchRequest).collect(Collectors.toList());
-    }
-
-    public static SearchRequest translateSearchRequest(CodescoopComponentSearch sw360Request) {
-        SearchRequest.Builder builder = SearchRequest
-                .build()
-                .searchQuery(sw360Request.searchQuery)
-                .ownerQuery(sw360Request.ownerQuery);
-
-        if (isNotBlank(sw360Request.getUuid())) {
-            builder.uuid(sw360Request.getUuid());
-        }
-        if (sw360Request.getLimit() > 1) {
-            builder.searchLimit(sw360Request.getLimit());
-        }
-        if (sw360Request.getOffset() > 0) {
-            builder.searchLimit(sw360Request.getOffset());
-        }
-
-        if (sw360Request.getFilter() != null) {
-            builder.filterType(sw360Request.getFilter().getType());
-            builder.filterLanguages(sw360Request.getFilter().getLanguages());
-            builder.filterLicenses(sw360Request.getFilter().getLicenses());
-            builder.filterMinScore((float) sw360Request.getFilter().getMinScore());
-        }
-
-        return builder.create();
-    }
-
-    public static List<CodescoopComponent> translateComponent(List<Component> external) {
-        return external.stream().map(ModelTranslator::translateComponent).collect(Collectors.toList());
-    }
-
-    public static CodescoopComponent translateComponent(Component external) {
-        CodescoopComponent c = new CodescoopComponent(
-                external.getId(),
-                external.getRate(),
-                external.getOwner(),
-                external.getName(),
-                external.getPurl(),
-                external.getType(),
-                external.getUuid());
-        if (external.getOrigin() != null) {
-            c.setOrigin(translateOrigin(external.getOrigin()));
-        }
-        return c;
-    }
-
-    public static CodescoopComponentOrigin translateOrigin(ComponentOrigin external) {
-        return new CodescoopComponentOrigin(
-                external.getGitId(),
-                external.getCreatedUTC(),
-                external.getOwner(),
-                external.getName(),
-                external.getTitle(),
-                external.getLicense(),
-                external.getUrl(),
-                external.getLogo(),
-                external.getAuthor(),
-                external.getMirrorUrl(),
-                external.getPrimaryLanguage(),
-                external.getHomepageUrl(),
-                external.getDiskUsage(),
-                external.getRate(),
-                external.isForked(),
-                external.isMirror(),
-                external.getComponentType(),
-                external.getLanguages(),
-                external.getTopics(),
-                external.getCsKeywords(),
-                external.getCsCompanies(),
-                translateCategory(external.getHierarchicalCategories()),
-                translateIndex(external.getCompositeIndex()));
-    }
-
-    public static CodescoopComponentCategory translateCategory(HierarchicalCategory external) {
-        return new CodescoopComponentCategory(external.getName(), external.getPath());
-    }
-
-    public static Set<CodescoopComponentCategory> translateCategory(Set<HierarchicalCategory> external) {
-        return external.stream().map(ModelTranslator::translateCategory).collect(Collectors.toSet());
-    }
-
-    public static CodescoopComponentIndex translateIndex(ComponentIndex external) {
-        return new CodescoopComponentIndex(
-                external.getInterestPercent(),
-                external.getActivityPercent(),
-                external.getHealthPercent());
-    }
-
-    public static CodescoopRelease translateRelease(Release external) {
-        return new CodescoopRelease(
-                external.getName(),
-                external.getVersion(),
-                external.getDate(),
-                external.getDateUTC(),
-                external.getDownloadUrl(),
-                external.getLicense());
-    }
-
-    public static List<CodescoopRelease> translateRelease(List<Release> external) {
-        return external.stream().map(ModelTranslator::translateRelease).collect(Collectors.toList());
-    }
-
-    public static AutocompleteRequest translateAutocompleteRequest(CodescoopAutocompleteRequest sw360Request) {
-        return AutocompleteRequest
-                .build()
-                .search(sw360Request.getSearch())
-                .limit(sw360Request.getLimit())
-                .by(sw360Request.getBy())
-                .create();
-    }
-
-    public static CodescoopAutocompleteComponet translateAutocompleteRepository(ComponentOrigin external) {
-        return new CodescoopAutocompleteComponet(
-                external.getId(),
-                external.getOwner(),
-                external.getName(),
-                external.getTitle(),
-                external.getUrl()
-        );
-    }
-
-    public static CodescoopAutocompleteResponse translateAutocompleteResponse(AutocompleteResponse external) {
-        CodescoopAutocompleteResponse response = new CodescoopAutocompleteResponse();
-        List<CodescoopComponent> repositories = external
-                .getRepositories()
-                .stream()
-                .map(repo -> translateComponent(repo))
-                .collect(Collectors.toList());
-        response.setRepositories(repositories);
-        return response;
-    }
-}

commit da677b5042b5c9654f3507d3e59386868c868fcd

fix(ui): fix issue #762, introduced in df54014
see also:
- https://github.com/eclipse/sw360/commit/df54014729a05e977095c187f65ce7f0d22f433f#commitcomment-36950304
- https://github.com/eclipse/sw360/issues/762

closes eclipse/sw360#762

Signed-off-by: Maximilian Huber <maximilian.huber@tngtech.com>

frontend/sw360-portlet/src/main/resources/META-INF/resources/html/components/view.jsp
@@ -63,7 +63,7 @@
     <portlet:param name="<%=PortalConstants.LICENSE_ID%>" value="<%=PortalConstants.FRIENDLY_URL_PLACEHOLDER_ID%>"/>
 </liferay-portlet:renderURL>
 
-div class="container" style="display: none;">
+<div class="container" style="display: none;">
 	<div class="row">
 		<div class="col-3 sidebar">
 			<div class="card-deck">
@@ -165,6 +165,12 @@ div class="container" style="display: none;">
 <%--for javascript library loading --%>
 <%@ include file="/html/utils/includes/requirejs.jspf" %>
 <script>
+    var renderCallback = function () {
+    };
+    var dataGetter = function(field) {
+    };
+</script>
+<script>
     AUI().use('liferay-portlet-url', function () {
         var PortletURL = Liferay.PortletURL;
 
@@ -215,13 +221,20 @@ div class="container" style="display: none;">
 
             // create and render data table
             function createComponentsTable() {
-                var columnDefs =  [];
+                let columns = [
+                    {"title": "Vendor", data: "vndrs"},
+                    {"title": "Component Name", data: "name", render: {display: renderComponentNameLink}},
+                    {"title": "Main Licenses", data: "lics", render: {display: renderLicenseLink}},
+                    {"title": "Component Type", data: "cType"},
+                    {"title": "Actions", data: "id", render: {display: renderComponentActions}, className: 'two actions', orderable: false }
+                ];
+                let printColumns = [0, 1, 2, 3];
                 var componentsTable = datatables.create('#componentsTable', {
                     bServerSide: true,
                     sAjaxSource: '<%=sw360ComponentsURL%>',
 
                     columns: columns,
-                    columnDefs: columnDefs,
+                    columnDefs: [],
                     drawCallback: renderCallback,
                     initComplete: datatables.showPageContainer,
                     order: [