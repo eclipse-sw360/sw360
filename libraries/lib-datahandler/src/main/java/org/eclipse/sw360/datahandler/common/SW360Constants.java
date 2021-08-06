4709a72973f4d03f4c047b0348117a24ddb193de
Pham Van Hieu <hieu1.phamvan@toshiba.co.jp>
2021-08-06 14:07:16 +0700

    Change SW360Constants TYPE_SPDX value

--- libraries/lib-datahandler/src/main/java/org/eclipse/sw360/datahandler/common/SW360Constants.java
+++ libraries/lib-datahandler/src/main/java/org/eclipse/sw360/datahandler/common/SW360Constants.java
@@ -70,7 +70,7 @@ public class SW360Constants {
     public static final String TYPE_SEARCHRESULT = "searchResult";
     public static final String TYPE_CHANGELOG = "changeLog";
     public static final String TYPE_VULNERABILITYDTO = "vulDTO";
-    public static final String TYPE_SPDX = "SPDX";
+    public static final String TYPE_SPDX = "SPDXDocument";
 
     /**
      * Hashmap containing the name field for each type.
