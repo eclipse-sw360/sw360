cb65f28d23c3d41e42bc87fb3d51d24b2d80c0d4
Smruti Prakash Sahoo <smruti.sahoo@siemens.com>
2021-09-15 17:55:42 +0530

    fix(projects): Projects loading issue with large records
    * The projects views crash with couchdb JS subsystem memory issue
    * Hence replaced the views with queries

--- backend/src-common/src/main/java/org/eclipse/sw360/datahandler/db/ProjectRepository.java
+++ backend/src-common/src/main/java/org/eclipse/sw360/datahandler/db/ProjectRepository.java
@@ -74,99 +74,6 @@ public class ProjectRepository extends SummaryAwareRepository<Project> {
                     "  }" +
                     "}";
 
-    private static final String FULL_MY_PROJECTS_VIEW =
-            "function(doc) {\n" +
-                    "  if (doc.type == 'project') {\n" +
-                    "    var acc = {};\n" +
-                    "    if(doc.createdBy)\n" +
-                    "      acc[doc.createdBy]=1;\n" +
-                    "    if(doc.leadArchitect)\n" +
-                    "      acc[doc.leadArchitect]=1;\n" +
-                    "    for(var i in doc.moderators) { \n" +
-                    "      acc[doc.moderators[i]]=1;\n" +
-                    "    }\n" +
-                    "    for(var i in doc.contributors) {\n" +
-                    "      acc[doc.contributors[i]]=1;\n" +
-                    "    }\n" +
-                    "    if(doc.projectOwner)\n" +
-                    "      acc[doc.projectOwner]=1;\n" +
-                    "    if(doc.projectResponsible)\n" +
-                    "      acc[doc.projectResponsible]=1;\n" +
-                    "    for(var i in doc.securityResponsibles) {\n" +
-                    "      acc[doc.securityResponsibles[i]]=1;\n" +
-                    "    }\n" +
-                    "    for(var i in acc){\n" +
-                    "      emit(i,doc);\n" +
-                    "    }\n" +
-                    "  }\n" +
-                    "}";
-
-    private static final String MY_ACCESSIBLE_PROJECTS_COUNT =
-            "function(doc) {\n" +
-            "  if (doc.type == 'project') {\n" +
-            "    var acc = {};\n" +
-            "    var meAndModerator = doc.visbility == \"ME_AND_MODERATORS\";\n" +
-            "    if(doc.createdBy && (meAndModerator || doc.visbility == \"PRIVATE\"))\n" +
-            "      acc[doc.createdBy]=1;\n" +
-            "    if(doc.leadArchitect && meAndModerator)\n" +
-            "      acc[doc.leadArchitect]=1;\n" +
-            "  if(meAndModerator) {\n" +
-            "    for(var i in doc.moderators) {\n" +
-            "        acc[doc.moderators[i]]=1;\n" +
-            "    }\n" +
-            "    if(meAndModerator) {\n" +
-            "    for(var i in doc.contributors) {\n" +
-            "        acc[doc.contributors[i]]=1;\n" +
-            "    }\n" +
-            "    }\n" +
-            "    if(doc.projectResponsible && meAndModerator)\n" +
-            "      acc[doc.projectResponsible]=1;\n" +
-            "    }\n" +
-            "    for(var i in acc){\n" +
-            "      emit(i,null);\n" +
-            "    }\n" +
-            "    if(doc.visbility == \"EVERYONE\"){\n" +
-            "      emit(\"everyone\",null)\n" +
-            "    }\n" +
-            "    if(doc.visbility == \"BUISNESSUNIT_AND_MODERATORS\") {\n" +
-            "        emit(doc.businessUnit,null);\n" +
-            "    }\n" +
-            "  }\n" +
-            "}";
-
-    private static final String ACCESSIBLE_PROJECTS_COUNT_FOR_CA_AND_ABOVE =
-            "function(doc) {\n" +
-            "  if (doc.type == 'project') {\n" +
-            "    var acc = {};\n" +
-            "    var meAndModerator = doc.visbility == \"ME_AND_MODERATORS\";\n" +
-            "    if(doc.createdBy && (meAndModerator || doc.visbility == \"PRIVATE\"))\n" +
-            "      acc[doc.createdBy]=1;\n" +
-            "    if(doc.leadArchitect && meAndModerator)\n" +
-            "      acc[doc.leadArchitect]=1;\n" +
-            "  if(meAndModerator) {\n" +
-            "    for(var i in doc.moderators) {\n" +
-            "        acc[doc.moderators[i]]=1;\n" +
-            "    }\n" +
-            "    if(meAndModerator) {\n" +
-            "    for(var i in doc.contributors) {\n" +
-            "        acc[doc.contributors[i]]=1;\n" +
-            "    }\n" +
-            "    }\n" +
-            "    if(doc.projectResponsible && meAndModerator)\n" +
-            "      acc[doc.projectResponsible]=1;\n" +
-            "    }\n" +
-            "    for(var i in acc){\n" +
-            "      emit(i,null);\n" +
-            "    }\n" +
-            "    if(doc.visbility == \"EVERYONE\"){\n" +
-            "      emit(\"everyone\",null)\n" +
-            "    }\n" +
-            "    if(doc.visbility == \"BUISNESSUNIT_AND_MODERATORS\") {\n" +
-            "        emit(\"bu\",null);\n" +
-            "    }\n" +
-            "  }\n" +
-            "}";
-
     private static final String BU_PROJECTS_VIEW =
             "function(doc) {" +
                     "  if (doc.type == 'project') {" +
@@ -272,13 +179,10 @@ public class ProjectRepository extends SummaryAwareRepository<Project> {
         views.put("bylinkingprojectid", createMapReduce(BY_LINKING_PROJECT_ID_VIEW, null));
         views.put("fullbylinkingprojectid", createMapReduce(FULL_BY_LINKING_PROJECT_ID_VIEW, null));
         views.put("myprojects", createMapReduce(MY_PROJECTS_VIEW, null));
-        views.put("fullmyprojects", createMapReduce(FULL_MY_PROJECTS_VIEW, null));
         views.put("buprojects", createMapReduce(BU_PROJECTS_VIEW, null));
         views.put("fullbuprojects", createMapReduce(FULL_BU_PROJECTS_VIEW, null));
         views.put("byexternalids", createMapReduce(BY_EXTERNAL_IDS, null));
         views.put("all", createMapReduce(ALL, null));
-        views.put("myfullprojectscount", createMapReduce(MY_ACCESSIBLE_PROJECTS_COUNT, "_count"));
-        views.put("myfullprojectscountca", createMapReduce(ACCESSIBLE_PROJECTS_COUNT_FOR_CA_AND_ABOVE, "_count"));
         initStandardDesignDocument(views, db);
         createIndex("byName", new String[] {"name"}, db);
         createIndex("byDesc", new String[] {"description"}, db);
@@ -339,7 +243,13 @@ public class ProjectRepository extends SummaryAwareRepository<Project> {
     }
 
     private Set<Project> getMyProjects(String user) {
-        return new HashSet<Project>(queryView("fullmyprojects", user));
+        QueryBuilder qb = new QueryBuilder(getMyProjectsSelector(user));
+        qb = qb.useIndex("byName");
+        qb = qb.sort(Sort.asc("name"));
+        String query = qb.build();
+        QueryResult<Project> queryResult = getConnector().getQueryResult(query, Project.class);
+        Set<Project> projects = new HashSet<Project>(queryResult.getDocs());
+        return projects;
     }
 
     public List<Project> getMyProjectsSummary(String user) {
@@ -385,63 +295,13 @@ public class ProjectRepository extends SummaryAwareRepository<Project> {
         final String userBU = getBUFromOrganisation(user.getDepartment());
         List<Project> projects = new ArrayList<>();
         Map<PaginationData, List<Project>> result = Maps.newHashMap();
-
-        String query = null;
-        final Selector typeSelector = eq("type", "project");
-        final Selector private_visibility_Selector = eq("visbility", "PRIVATE");
-        final Selector createdBySelector = eq("createdBy", requestingUserEmail);
-        final Selector getAllPrivateProjects = and(private_visibility_Selector, createdBySelector);
-        final Selector everyone_visibility_Selector = eq("visbility", "EVERYONE");
-
-        final Selector isAProjectResponsible = eq("projectResponsible", requestingUserEmail);
-        final Selector isALeadArchitect = eq("leadArchitect", requestingUserEmail);
-        final Selector isAModerator = PredicatedOperation.elemMatch("moderators",
-                PredicateExpression.eq(requestingUserEmail));
-        final Selector isAContributor = PredicatedOperation.elemMatch("contributors",
-                PredicateExpression.eq(requestingUserEmail));
-        final Selector meAndModorator_visibility_Selector = eq("visbility", "ME_AND_MODERATORS");
-        final Selector isUserBelongToMeAndModerator = and(meAndModorator_visibility_Selector,
-                or(createdBySelector, isAProjectResponsible, isALeadArchitect, isAModerator, isAContributor));
-
-        final Selector buAndModorator_visibility_Selector = eq("visbility", "BUISNESSUNIT_AND_MODERATORS");
-        final Selector userBuSelector = eq("businessUnit", userBU);
-        boolean isClearingAdmin = PermissionUtils.isUserAtLeast(UserGroup.CLEARING_ADMIN, user);
-        Selector isUserBelongToBuAndModerator = null;
-
-        Selector[] buSelectors = null;
-        Map<String, Set<UserGroup>> secondaryDepartmentsAndRoles = user.getSecondaryDepartmentsAndRoles();
-        if (!CommonUtils.isNullOrEmptyMap(secondaryDepartmentsAndRoles)) {
-            Set<String> secondaryUgs = secondaryDepartmentsAndRoles.keySet();
-            Set<String> secondaryBus = secondaryUgs.stream().map(SW360Utils::getBUFromOrganisation)
-                    .collect(Collectors.toSet());
-            buSelectors = new Selector[secondaryBus.size() + 2];
-            int index = 0;
-            for (String secondaryBU : secondaryBus) {
-                Selector buselector = eq("businessUnit", secondaryBU);
-                buSelectors[index] = buselector;
-                index++;
-            }
-        } else {
-            buSelectors = new Selector[2];
-        }
-        buSelectors[buSelectors.length - 2] = isUserBelongToMeAndModerator;
-        buSelectors[buSelectors.length - 1] = userBuSelector;
-        isUserBelongToBuAndModerator = and(buAndModorator_visibility_Selector, or(buSelectors));
-
-        Selector finalSelector = null;
-        if (isClearingAdmin) {
-            finalSelector = and(typeSelector, or(getAllPrivateProjects, everyone_visibility_Selector,
-                    isUserBelongToMeAndModerator, buAndModorator_visibility_Selector));
-        } else {
-            finalSelector = and(typeSelector, or(getAllPrivateProjects, everyone_visibility_Selector,
-                    isUserBelongToMeAndModerator, isUserBelongToBuAndModerator));
-        }
-
+        Selector finalSelector = getMangoQuerySelectorForProject(user, requestingUserEmail, userBU);
         QueryBuilder qb = new QueryBuilder(finalSelector);
         if (rowsPerPage != -1) {
             qb.limit(rowsPerPage);
         }
         qb.skip(pageData.getDisplayStart());
+        String query = null;
         ViewRequestBuilder queryView = null;
         switch (sortColumnNo) {
         case 0:
@@ -546,33 +406,84 @@ public class ProjectRepository extends SummaryAwareRepository<Project> {
     }
 
     public int getMyAccessibleProjectsCount(User user) {
-        boolean isClearingAdmin = PermissionUtils.isUserAtLeast(UserGroup.CLEARING_ADMIN, user);
-        Set<String> BUs = new HashSet<>();
-        String primaryOrg = SW360Utils.getBUFromOrganisation(user.getDepartment());
-        BUs.add(primaryOrg);
+        final String requestingUserEmail = user.getEmail();
+        final String userBU = getBUFromOrganisation(user.getDepartment());
+        List<Project> projects = new ArrayList<>();
+        Selector finalSelector = getMangoQuerySelectorForProject(user, requestingUserEmail, userBU);
+        QueryBuilder qb = new QueryBuilder(finalSelector);
+        qb = qb.fields("_id").useIndex("byName").sort(Sort.asc("name"));
+        QueryResult<Project> queryResult = getConnector().getQueryResult(qb.build(), Project.class);
+        projects = queryResult.getDocs();
+        return projects.size();
+    }
 
+    private Selector getMangoQuerySelectorForProject(User user, final String requestingUserEmail, final String userBU) {
+        final Selector typeSelector = eq("type", "project");
+        final Selector private_visibility_Selector = eq("visbility", "PRIVATE");
+        final Selector createdBySelector = eq("createdBy", requestingUserEmail);
+        final Selector getAllPrivateProjects = and(private_visibility_Selector, createdBySelector);
+        final Selector everyone_visibility_Selector = eq("visbility", "EVERYONE");
+
+        final Selector isAProjectResponsible = eq("projectResponsible", requestingUserEmail);
+        final Selector isALeadArchitect = eq("leadArchitect", requestingUserEmail);
+        final Selector isAModerator = PredicatedOperation.elemMatch("moderators",
+                PredicateExpression.eq(requestingUserEmail));
+        final Selector isAContributor = PredicatedOperation.elemMatch("contributors",
+                PredicateExpression.eq(requestingUserEmail));
+        final Selector meAndModorator_visibility_Selector = eq("visbility", "ME_AND_MODERATORS");
+        final Selector isUserBelongToMeAndModerator = and(meAndModorator_visibility_Selector,
+                or(createdBySelector, isAProjectResponsible, isALeadArchitect, isAModerator, isAContributor));
+
+        final Selector buAndModorator_visibility_Selector = eq("visbility", "BUISNESSUNIT_AND_MODERATORS");
+        final Selector userBuSelector = eq("businessUnit", userBU);
+        boolean isClearingAdmin = PermissionUtils.isUserAtLeast(UserGroup.CLEARING_ADMIN, user);
+        Selector isUserBelongToBuAndModerator = null;
+
+        Selector[] buSelectors = null;
         Map<String, Set<UserGroup>> secondaryDepartmentsAndRoles = user.getSecondaryDepartmentsAndRoles();
         if (!CommonUtils.isNullOrEmptyMap(secondaryDepartmentsAndRoles)) {
-            Set<String> secondaryUGs = secondaryDepartmentsAndRoles.keySet();
-            Set<String> secondaryBUs = secondaryUGs.stream().map(SW360Utils::getBUFromOrganisation)
+            Set<String> secondaryUgs = secondaryDepartmentsAndRoles.keySet();
+            Set<String> secondaryBus = secondaryUgs.stream().map(SW360Utils::getBUFromOrganisation)
                     .collect(Collectors.toSet());
-            BUs.addAll(secondaryBUs);
+            buSelectors = new Selector[secondaryBus.size() + 2];
+            int index = 0;
+            for (String secondaryBU : secondaryBus) {
+                Selector buselector = eq("businessUnit", secondaryBU);
+                buSelectors[index] = buselector;
+                index++;
+            }
+        } else {
+            buSelectors = new Selector[2];
         }
+        buSelectors[buSelectors.length - 2] = isUserBelongToMeAndModerator;
+        buSelectors[buSelectors.length - 1] = userBuSelector;
+        isUserBelongToBuAndModerator = and(buAndModorator_visibility_Selector, or(buSelectors));
 
-        String[] keys = new String[BUs.size() + 2];
-        int index = 0;
-        for (String str : BUs) {
-            keys[index++] = str;
-        }
-        keys[keys.length - 2] = user.getEmail();
-        keys[keys.length - 1] = "everyone";
+        Selector finalSelector = null;
         if (isClearingAdmin) {
-            String[] keyss = new String[3];
-            keyss[keyss.length - 3] = "bu";
-            keyss[keyss.length - 2] = user.getEmail();
-            keyss[keyss.length - 1] = "everyone";
-            return getConnector().getDocumentCount(Project.class, "myfullprojectscountca", keyss);
+            finalSelector = and(typeSelector, or(getAllPrivateProjects, everyone_visibility_Selector,
+                    isUserBelongToMeAndModerator, buAndModorator_visibility_Selector));
+        } else {
+            finalSelector = and(typeSelector, or(getAllPrivateProjects, everyone_visibility_Selector,
+                    isUserBelongToMeAndModerator, isUserBelongToBuAndModerator));
         }
-        return getConnector().getDocumentCount(Project.class, "myfullprojectscount", keys);
+        return finalSelector;
+    }
+
+    private Selector getMyProjectsSelector(final String requestingUserEmail) {
+        final Selector typeSelector = eq("type", "project");
+        final Selector createdBySelector = eq("createdBy", requestingUserEmail);
+        final Selector isAProjectResponsible = eq("projectResponsible", requestingUserEmail);
+        final Selector isProjectOwner = eq("projectOwner", requestingUserEmail);
+        final Selector isSecurityResponsible = PredicatedOperation.elemMatch("securityResponsibles",
+                PredicateExpression.eq(requestingUserEmail));
+        final Selector isALeadArchitect = eq("leadArchitect", requestingUserEmail);
+        final Selector isAModerator = PredicatedOperation.elemMatch("moderators",
+                PredicateExpression.eq(requestingUserEmail));
+        final Selector isAContributor = PredicatedOperation.elemMatch("contributors",
+                PredicateExpression.eq(requestingUserEmail));
+        Selector finalSelector = and(typeSelector, or(createdBySelector, isProjectOwner, isAProjectResponsible,
+                isALeadArchitect, isAModerator, isAContributor, isSecurityResponsible));
+        return finalSelector;
     }
 }
