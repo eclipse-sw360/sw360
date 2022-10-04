01eecf3a01c2eeca3ec86623d70ab65e42883a8e
akapti <abdul.kapti@siemens-healthineers.com>
2022-10-04 12:34:59 +0530

    fix(ProjectUI): Changing Project group should update CR
    Signed-off-by: akapti <abdul.kapti@siemens-healthineers.com>

--- backend/src/src-moderation/src/main/java/org/eclipse/sw360/moderation/db/ModerationDatabaseHandler.java
+++ backend/src/src-moderation/src/main/java/org/eclipse/sw360/moderation/db/ModerationDatabaseHandler.java
@@ -275,6 +275,16 @@ public class ModerationDatabaseHandler {
         clearingRequestRepository.update(clearingRequest);
     }
 
+    public void updateClearingRequestForChangeInProjectBU(String crId, String businessUnit, User user) {
+        try {
+            ClearingRequest clearingRequest = clearingRequestRepository.get(crId);
+            clearingRequest.setProjectBU(businessUnit);
+            clearingRequestRepository.update(clearingRequest);
+        } catch (Exception e) {
+            log.error("Failed to update CR-ID: %s with error: %s", crId, e.getMessage());
+        }
+    }
+
     public RequestStatus addCommentToClearingRequest(String id, Comment comment, User user) {
         try {
             comment.setCommentedOn(System.currentTimeMillis());
