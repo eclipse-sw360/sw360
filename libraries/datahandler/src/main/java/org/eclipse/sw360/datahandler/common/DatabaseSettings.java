35370dbf51ac4a6088f7de0656dbbb0ede258336
akapti <abdul.kapti@siemens-healthineers.com>
2022-10-21 12:50:51 +0530

    fix(chores):connect to couchdb with custom properties
    Signed-off-by: akapti <abdul.kapti@siemens-healthineers.com>

--- libraries/datahandler/src/main/java/org/eclipse/sw360/datahandler/common/DatabaseSettings.java
+++ libraries/datahandler/src/main/java/org/eclipse/sw360/datahandler/common/DatabaseSettings.java
@@ -51,23 +51,20 @@ public class DatabaseSettings {
     public static boolean LUCENE_LEADING_WILDCARD = false;
 
     static {
-        File propertyFile = new File(PROPERTIES_FILE_PATH);
-        if (propertyFile.exists()) {
-            Properties props = CommonUtils.loadProperties(DatabaseSettings.class, PROPERTIES_FILE_PATH);
+        Properties props = CommonUtils.loadProperties(DatabaseSettings.class, PROPERTIES_FILE_PATH);
 
-            COUCH_DB_URL = props.getProperty("couchdb.url", "http://localhost:5984");
-            COUCH_DB_LUCENE_URL = props.getProperty("couchdb.lucene.url", "http://localhost:8080/couchdb-lucene");
-            COUCH_DB_DATABASE = props.getProperty("couchdb.database", "sw360db");
-            COUCH_DB_USERNAME = props.getProperty("couchdb.user");
-            COUCH_DB_PASSWORD = props.getProperty("couchdb.password");
-            COUCH_DB_ATTACHMENTS = props.getProperty("couchdb.attachments", "sw360attachments");
-            COUCH_DB_CHANGE_LOGS = props.getProperty("couchdb.change_logs", "sw360changelogs");
-            COUCH_DB_CONFIG = props.getProperty("couchdb.config", "sw360config");
-            COUCH_DB_USERS = props.getProperty("couchdb.usersdb", "sw360users");
-            COUCH_DB_VM = props.getProperty("couchdb.vulnerability_management", "sw360vm");
-            LUCENE_SEARCH_LIMIT = Integer.parseInt(props.getProperty("lucenesearch.limit", "25"));
-            LUCENE_LEADING_WILDCARD = false;
-        }
+        COUCH_DB_URL = props.getProperty("couchdb.url", "http://localhost:5984");
+        COUCH_DB_LUCENE_URL = props.getProperty("couchdb.lucene.url", "http://localhost:8080/couchdb-lucene");
+        COUCH_DB_DATABASE = props.getProperty("couchdb.database", "sw360db");
+        COUCH_DB_USERNAME = props.getProperty("couchdb.user", "admin");
+        COUCH_DB_PASSWORD = props.getProperty("couchdb.password", "password");
+        COUCH_DB_ATTACHMENTS = props.getProperty("couchdb.attachments", "sw360attachments");
+        COUCH_DB_CHANGE_LOGS = props.getProperty("couchdb.change_logs", "sw360changelogs");
+        COUCH_DB_CONFIG = props.getProperty("couchdb.config", "sw360config");
+        COUCH_DB_USERS = props.getProperty("couchdb.usersdb", "sw360users");
+        COUCH_DB_VM = props.getProperty("couchdb.vulnerability_management", "sw360vm");
+        LUCENE_SEARCH_LIMIT = Integer.parseInt(props.getProperty("lucenesearch.limit", "25"));
+        LUCENE_LEADING_WILDCARD = Boolean.parseBoolean(props.getProperty("lucenesearch.leading.wildcard", "false"));
     }
 
     public static Supplier<HttpClient> getConfiguredHttpClient() throws MalformedURLException {
