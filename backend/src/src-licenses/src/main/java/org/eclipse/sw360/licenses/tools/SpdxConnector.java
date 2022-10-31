06b741b09e4bb63e823bc1a699060effac13b0cf
tuannn2 <tuan2.nguyennhu@toshiba.co.jp>
2022-10-31 15:46:26 +0700

    fix(SPDX): import SPDX licenses with new SPDX library (tools-java 1.0.4)
    Signed-off-by: Nguyen Nhu Tuan <tuan2.nguyennhu@toshiba.co.jp>

--- backend/src/src-licenses/src/main/java/org/eclipse/sw360/licenses/tools/SpdxConnector.java
+++ backend/src/src-licenses/src/main/java/org/eclipse/sw360/licenses/tools/SpdxConnector.java
@@ -15,11 +15,11 @@ import org.apache.logging.log4j.Logger;
 import org.eclipse.sw360.datahandler.thrift.SW360Exception;
 import org.eclipse.sw360.datahandler.thrift.licenses.License;
 import org.eclipse.sw360.datahandler.thrift.Quadratic;
-import org.spdx.compare.LicenseCompareHelper;
-import org.spdx.compare.SpdxCompareException;
-import org.spdx.rdfparser.InvalidSPDXAnalysisException;
-import org.spdx.rdfparser.license.LicenseInfoFactory;
-import org.spdx.rdfparser.license.SpdxListedLicense;
+import org.spdx.library.InvalidSPDXAnalysisException;
+import org.spdx.library.model.license.LicenseInfoFactory;
+import org.spdx.library.model.license.SpdxListedLicense;
+import org.spdx.utility.compare.LicenseCompareHelper;
+import org.spdx.utility.compare.SpdxCompareException;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -32,7 +32,8 @@ public class SpdxConnector {
     private static final Logger log = LogManager.getLogger(SpdxConnector.class);
 
     public static List<String> getAllSpdxLicenseIds() {
-        return Arrays.asList(LicenseInfoFactory.getSpdxListedLicenseIds());
+        return LicenseInfoFactory.getSpdxListedLicenseIds();
+
     }
 
     protected static Optional<SpdxListedLicense> getSpdxLicense(String licenseId) {
@@ -51,20 +52,24 @@ public class SpdxConnector {
                 .flatMap(SpdxConnector::getSpdxLicenseAsSW360License);
     }
 
-    public static Optional<License> getSpdxLicenseAsSW360License(SpdxListedLicense spdxListedLicense){
-        Quadratic isOSIApproved = spdxListedLicense.isOsiApproved() ? Quadratic.YES : Quadratic.NA;
-        Quadratic isFSFLibre = spdxListedLicense.isFsfLibre() ? Quadratic.YES : Quadratic.NA;
+    public static Optional<License> getSpdxLicenseAsSW360License(SpdxListedLicense spdxListedLicense) {
+        try {
+            Quadratic isOSIApproved = spdxListedLicense.isOsiApproved() ? Quadratic.YES : Quadratic.NA;
+            Quadratic isFSFLibre = spdxListedLicense.isFsfLibre() ? Quadratic.YES : Quadratic.NA;
 
-        License license = new License()
-                .setId(spdxListedLicense.getLicenseId())
-                .setShortname(spdxListedLicense.getLicenseId())
-                .setFullname(spdxListedLicense.getName())
-                .setText(spdxListedLicense.getLicenseText())
-                .setOSIApproved(isOSIApproved)
-                .setFSFLibre(isFSFLibre)
-                .setExternalLicenseLink("https://spdx.org/licenses/" + spdxListedLicense.getLicenseId()+ ".html")
-                .setExternalIds(Collections.singletonMap("SPDX-License-Identifier", spdxListedLicense.getLicenseId()));
-        return Optional.of(license);
+            License license = new License()
+                    .setId(spdxListedLicense.getLicenseId())
+                    .setShortname(spdxListedLicense.getLicenseId())
+                    .setFullname(spdxListedLicense.getName())
+                    .setText(spdxListedLicense.getLicenseText())
+                    .setOSIApproved(isOSIApproved)
+                    .setFSFLibre(isFSFLibre)
+                    .setExternalLicenseLink("https://spdx.org/licenses/" + spdxListedLicense.getLicenseId()+ ".html")
+                    .setExternalIds(Collections.singletonMap("SPDX-License-Identifier", spdxListedLicense.getLicenseId()));
+            return Optional.of(license);
+        } catch (InvalidSPDXAnalysisException e) {
+            throw new RuntimeException("Error get spdx license: "+e.getMessage());
+        }
     }
 
     /**
@@ -72,11 +77,11 @@ public class SpdxConnector {
      * @param license
      * @return
      */
-    public static boolean matchesSpdxLicenseText(License license) {
+    public static boolean matchesSpdxLicenseText(License license) throws InvalidSPDXAnalysisException {
         return matchesSpdxLicenseText(license, license.getShortname());
     }
 
-    public static boolean matchesSpdxLicenseText(License license, String spdxId) {
+    public static boolean matchesSpdxLicenseText(License license, String spdxId) throws InvalidSPDXAnalysisException {
         final Optional<SpdxListedLicense> spdxLicense = getSpdxLicense(spdxId);
         if(!spdxLicense.isPresent()) {
             // A license, which has no SPDX counterpart, does not not match its SPDX-license
@@ -85,7 +90,7 @@ public class SpdxConnector {
         return matchesSpdxLicenseText(license, spdxLicense.get());
     }
 
-    private static boolean matchesSpdxLicenseText(License license, SpdxListedLicense spdxLicense) {
+    private static boolean matchesSpdxLicenseText(License license, SpdxListedLicense spdxLicense) throws InvalidSPDXAnalysisException {
         return matchesLicenseText(license, spdxLicense.getLicenseText());
     }
 
