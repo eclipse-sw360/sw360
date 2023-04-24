e1dd33f43c680d0025fb0e4cb1acfcb2d4f5c914
Gaurav Mishra <gmishx@gmail.com>
2023-04-24 14:39:49 +0530

    fix(rest): added endpoint for release subscriptions
    Add endpoint to list subscriptions of releases.
    
    Signed-off-by: Gaurav Mishra <gmishx@gmail.com>

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/release/Sw360ReleaseService.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/release/Sw360ReleaseService.java
@@ -134,6 +134,11 @@ public class Sw360ReleaseService implements AwareOfRestServices<Release> {
         return releaseById;
     }
 
+    public List<Release> getReleaseSubscriptions(User sw360User) throws TException {
+        ComponentService.Iface sw360ComponentClient = getThriftComponentClient();
+        return sw360ComponentClient.getSubscribedReleases(sw360User);
+    }
+
     @Override
     public Set<Release> searchByExternalIds(Map<String, Set<String>> externalIds, User user) throws TException {
         ComponentService.Iface sw360ComponentClient = getThriftComponentClient();
