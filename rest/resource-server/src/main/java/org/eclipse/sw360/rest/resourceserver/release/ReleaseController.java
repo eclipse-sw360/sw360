9d37c49937c8e9931b0ed95e5b5731b92fbc37aa
Kouki Hama <kouki1.hama@toshiba.co.jp>
2023-08-22 17:36:09 +0900

    fix(rest) : Modifications to improve GET result speed

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/release/ReleaseController.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/release/ReleaseController.java
@@ -150,10 +150,6 @@ public class ReleaseController implements RepresentationModelProcessor<Repositor
             sw360Releases.addAll(releaseService.getReleasesForUser(sw360User));
         }
 
-        for(Release release: sw360Releases) {
-            releaseService.setComponentDependentFieldsInRelease(release, sw360User);
-        }
-
         sw360Releases = sw360Releases.stream()
                 .filter(release -> name == null || name.isEmpty() || release.getName().equalsIgnoreCase(name))
                 .collect(Collectors.toList());
@@ -164,6 +160,15 @@ public class ReleaseController implements RepresentationModelProcessor<Repositor
                 }
             }
         }
+        
+        if (CommonUtils.isNotNullEmptyOrWhitespace(sha1) || CommonUtils.isNotNullEmptyOrWhitespace(name)) {
+            for(Release release: sw360Releases) {
+                releaseService.setComponentDependentFieldsInRelease(release, sw360User);
+            }
+        } else {
+            releaseService.setComponentDependentFieldsInRelease(sw360Releases, sw360User);
+        }
+        
         PaginationResult<Release> paginationResult = restControllerHelper.createPaginationResult(request, pageable, sw360Releases, SW360Constants.TYPE_RELEASE);
 
         List<EntityModel> releaseResources = new ArrayList<>();
