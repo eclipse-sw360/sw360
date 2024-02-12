be7606f32eb14ccc3ce30de01f619deccb4a28d7
nikesh kumar <kumar.nikesh@siemens.com>
2024-02-12 12:36:12 +0530

    feat(rest): create new enpoint to upload component csv file.
    Signed-off-by: Nikesh kumar <kumar.nikesh@siemens.com>

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/importexport/ImportExportController.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/importexport/ImportExportController.java
@@ -16,10 +16,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import java.io.IOException;
 
 import org.apache.thrift.TException;
+import org.apache.thrift.transport.TTransportException;
 import org.eclipse.sw360.datahandler.thrift.users.User;
 import org.eclipse.sw360.rest.resourceserver.core.RestControllerHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.eclipse.sw360.datahandler.common.CommonUtils;
+import org.eclipse.sw360.datahandler.thrift.RequestStatus;
+import org.eclipse.sw360.datahandler.thrift.RequestSummary;
+import org.eclipse.sw360.rest.resourceserver.project.ProjectController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
@@ -38,6 +43,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.HttpStatus.Series;
+import org.springframework.http.MediaType;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
@@ -47,10 +59,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @RestController
 @SecurityRequirement(name = "tokenAuth")
-public class ImportExportController implements  RepresentationModelProcessor<RepositoryLinksResource> {
+public class ImportExportController implements RepresentationModelProcessor<RepositoryLinksResource> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ImportExportController.class);
     public static final String IMPORTEXPORT_URL = "/importExport";
+
+    private static final MediaType form = null;
 
     @NonNull
     private final RestControllerHelper restControllerHelper;
@@ -114,7 +128,7 @@ public class ImportExportController implements  RepresentationModelProcessor<Rep
     )
     @PreAuthorize("hasAuthority('WRITE')")
     @GetMapping(value = IMPORTEXPORT_URL + "/downloadAttachmentInfo")
-    public void downloadAttachmentInfo(HttpServletResponse response) {
+    public void downloadAttachmentInfo(HttpServletResponse response) throws TTransportException{
         try {
             User sw360User = restControllerHelper.getSw360UserFromAuthentication();
             importExportService.getDownloadAttachmentInfo(sw360User, response);
@@ -197,5 +211,65 @@ public class ImportExportController implements  RepresentationModelProcessor<Rep
     public ResponseEntity<String> handleGlobalException(Exception e) {
         LOGGER.error("Unhandled exception: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @Operation(
+            summary = "Upload component CSV file.",
+            description = "Upload a component CSV file to the system.",
+            tags = {"ImportExport"},
+            parameters = {
+                @Parameter(name = "Content-Type", in = ParameterIn.HEADER, required = true, description = "The content type of the request. Supported values: multipart/mixed or multipart/form-data.")
+            }
+        )
+    @RequestMapping(value = IMPORTEXPORT_URL + "/uploadComponent", method = RequestMethod.POST, consumes = {
+            MediaType.MULTIPART_MIXED_VALUE,
+            MediaType.MULTIPART_FORM_DATA_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<RequestSummary> uploadComponentCsv(
+            @Parameter(description = "The component csv file to be uploaded.") @RequestParam("componentFile") MultipartFile file,
+            HttpServletRequest request, HttpServletResponse response) throws TException, IOException, ServletException {
+
+        User sw360User = restControllerHelper.getSw360UserFromAuthentication();
+        RequestSummary requestSummary = importExportService.uploadComponent(sw360User, file, request, response);
+        return ResponseEntity.ok(requestSummary);
+    }
+
+    @Operation(
+            summary = "Release link file.",
+            description = "Release link file.",
+            tags = {"ImportExport"},
+            parameters = {
+                    @Parameter(name = "Content-Type", in = ParameterIn.HEADER, required = true,  description = "The content type of the request. Supported values: multipart/mixed or multipart/form-data."),
+            }
+    )
+    @RequestMapping(value = IMPORTEXPORT_URL + "/uploadRelease", method = RequestMethod.POST, consumes = {
+            MediaType.MULTIPART_MIXED_VALUE,
+            MediaType.MULTIPART_FORM_DATA_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<RequestSummary> uploadReleaseCsv(
+            @Parameter(description = "The release csv file to be uploaded.") @RequestParam("releaseFile") MultipartFile file,
+            HttpServletRequest request, HttpServletResponse response) throws TException, IOException, ServletException {
+
+        User sw360User = restControllerHelper.getSw360UserFromAuthentication();
+        RequestSummary requestSummary = importExportService.uploadReleaseLink(sw360User, file, request);
+        return ResponseEntity.ok(requestSummary);
+    }
+
+    @Operation(
+            summary = "Component attachment file.",
+            description = "Component attachment file.",
+            tags = {"ImportExport"},
+            parameters = {
+                    @Parameter(name = "Content-Type", in = ParameterIn.HEADER, required = true,  description = "The content type of the request. Supported values: multipart/mixed or multipart/form-data."),
+            }
+    )
+    @RequestMapping(value = IMPORTEXPORT_URL + "/componentAttachment", method = RequestMethod.POST, consumes = {
+            MediaType.MULTIPART_MIXED_VALUE,
+            MediaType.MULTIPART_FORM_DATA_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<RequestSummary> uploadComponentAttachment(
+            @Parameter(description = "The component attachment csv file to be uploaded.") @RequestParam("attachmentFile") MultipartFile file,
+            HttpServletRequest request, HttpServletResponse response) throws TException, IOException, ServletException {
+
+        User sw360User = restControllerHelper.getSw360UserFromAuthentication();
+        RequestSummary requestSummary = importExportService.uploadComponentAttachment(sw360User, file, request);
+        return ResponseEntity.ok(requestSummary);
     }
 }
