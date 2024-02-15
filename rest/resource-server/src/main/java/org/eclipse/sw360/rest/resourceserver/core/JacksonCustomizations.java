fba0d8e52f152c1eb14f3746e2c4081da0592e6c
Nikesh kumar <kumar.nikesh@simens.com>
2024-02-15 12:09:01 +0530

    fix(rest): Added modifiedBy field in project search by id.
    Signed-off-by: Nikesh kumar <kumar.nikesh@simens.com>

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/core/JacksonCustomizations.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/core/JacksonCustomizations.java
@@ -269,7 +269,6 @@ public class JacksonCustomizations {
                 "setSpdxId",
                 "setModifiedOn",
                 "setModifiedBy",
-                "modifiedBy",
                 "packageIdsSize",
                 "setPackageIds",
                 "packageIdsIterator",
