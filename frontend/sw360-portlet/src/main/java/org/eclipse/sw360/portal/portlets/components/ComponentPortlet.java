df735e9b2ef8662e7d69dfdeeba475bd5359ac33
Eldrin <eldrin.sanctis@siemens.com>
2024-04-16 13:43:41 +0200

    fix(Release): Updating the license overview in the summary page

--- frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/portlets/components/ComponentPortlet.java
+++ frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/portlets/components/ComponentPortlet.java
@@ -902,6 +902,7 @@ public class ComponentPortlet extends FossologyAwarePortlet {
             JsonNode input = OBJECT_MAPPER.readValue(request.getParameter(SPDX_LICENSE_INFO), JsonNode.class);
             JsonNode licenesIdsNode = input.get(LICENSE_IDS);
             if (null != licenesIdsNode) {
+                release.getMainLicenseIds().clear();
                 if (licenesIdsNode.isArray()) {
                     for (JsonNode objNode : licenesIdsNode) {
                         release.addToMainLicenseIds(objNode.asText());
@@ -912,6 +913,7 @@ public class ComponentPortlet extends FossologyAwarePortlet {
             }
             licenesIdsNode = input.get("otherLicenseIds");
             if (null != licenesIdsNode) {
+                release.getOtherLicenseIds().clear();
                 if (licenesIdsNode.isArray()) {
                     for (JsonNode objNode : licenesIdsNode) {
                         release.addToOtherLicenseIds(objNode.asText());
