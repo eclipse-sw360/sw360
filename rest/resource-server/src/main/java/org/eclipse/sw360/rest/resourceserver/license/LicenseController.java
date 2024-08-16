2lambda123/toshiba-sw360

feat(License): Add API Listing LicenseType and Add pageble for licenses, obligations

rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/license/LicenseController.java
@@ -20,6 +20,10 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
+import org.eclipse.sw360.datahandler.common.SW360Constants;
+import org.eclipse.sw360.datahandler.resourcelists.PaginationParameterException;
+import org.eclipse.sw360.datahandler.resourcelists.PaginationResult;
+import org.eclipse.sw360.datahandler.resourcelists.ResourceClassNotFoundException;
 import org.eclipse.sw360.datahandler.thrift.RequestStatus;
 import org.eclipse.sw360.datahandler.thrift.RequestSummary;
 import org.eclipse.sw360.datahandler.thrift.licenses.License;
@@ -29,6 +33,7 @@ import org.eclipse.sw360.datahandler.thrift.users.User;
 import org.eclipse.sw360.rest.resourceserver.core.HalResource;
 import org.eclipse.sw360.rest.resourceserver.core.RestControllerHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
 import org.springframework.hateoas.CollectionModel;
@@ -48,6 +53,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -62,6 +68,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @SecurityRequirement(name = "tokenAuth")
 public class LicenseController implements RepresentationModelProcessor<RepositoryLinksResource> {
     public static final String LICENSES_URL = "/licenses";
+    public static final String LICENSE_TYPES_URL = "/licenseTypes";
 
     @NonNull
     private final Sw360LicenseService licenseService;
@@ -78,17 +85,22 @@ public class LicenseController implements RepresentationModelProcessor<Repositor
             tags = {"Licenses"}
     )
     @RequestMapping(value = LICENSES_URL, method = RequestMethod.GET)
-    public ResponseEntity<CollectionModel<EntityModel<License>>> getLicenses() throws TException {
+    public ResponseEntity<CollectionModel> getLicenses(Pageable pageable, HttpServletRequest request) throws TException, ResourceClassNotFoundException, PaginationParameterException, URISyntaxException {
         List<License> sw360Licenses = licenseService.getLicenses();
-
+        PaginationResult<License> paginationResult = restControllerHelper.createPaginationResult(request, pageable, sw360Licenses, SW360Constants.TYPE_LICENSE);
         List<EntityModel<License>> licenseResources = new ArrayList<>();
-        for (License sw360License : sw360Licenses) {
-            License embeddedLicense = restControllerHelper.convertToEmbeddedLicense(sw360License);
-            EntityModel<License> licenseResource = EntityModel.of(embeddedLicense);
-            licenseResources.add(licenseResource);
+        paginationResult.getResources().stream()
+                .forEach(license -> {
+                    License embeddedLicense = restControllerHelper.convertToEmbeddedLicense(license);
+                    EntityModel<License> licenseResource = EntityModel.of(embeddedLicense);
+                    licenseResources.add(licenseResource);
+                });
+        CollectionModel resources;
+        if (licenseResources.size() == 0) {
+            resources = restControllerHelper.emptyPageResource(License.class, paginationResult);
+        } else {
+            resources = restControllerHelper.generatePagesResource(paginationResult, licenseResources);
         }
-
-        CollectionModel<EntityModel<License>> resources = CollectionModel.of(licenseResources);
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
@@ -102,6 +114,20 @@ public class LicenseController implements RepresentationModelProcessor<Repositor
             obligationResources.add(EntityModel.of(embeddedObligation));
         });
         CollectionModel<EntityModel<Obligation>> resources = CollectionModel.of(obligationResources);
+        return new ResponseEntity<>(resources, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = LICENSE_TYPES_URL, method = RequestMethod.GET)
+    public ResponseEntity<CollectionModel<EntityModel<LicenseType>>> getLicenseTypes() throws TException {
+        List<LicenseType> sw360LicenseTypes = licenseService.getLicenseTypes();
+
+        List<EntityModel<LicenseType>> licenseTypeResources = new ArrayList<>();
+        for (LicenseType sw360LicenseType : sw360LicenseTypes) {
+            LicenseType embeddedLicenseType = restControllerHelper.convertToEmbeddedLicenseType(sw360LicenseType);
+            EntityModel<LicenseType> licenseResource = EntityModel.of(embeddedLicenseType);
+            licenseTypeResources.add(licenseResource);
+        }
+        CollectionModel<EntityModel<LicenseType>> resources = CollectionModel.of(licenseTypeResources);
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
@@ -148,6 +174,10 @@ public class LicenseController implements RepresentationModelProcessor<Repositor
             @RequestBody License license
     ) throws TException {
         User sw360User = restControllerHelper.getSw360UserFromAuthentication();
+        List<License> sw360Licenses = licenseService.getLicenses();
+        if(restControllerHelper.checkDuplicateLicense(sw360Licenses, license.shortname)) {
+            return new ResponseEntity("sw360 component with name" + license.shortname +"already exists.", HttpStatus.CONFLICT);
+        }
         license = licenseService.createLicense(license, sw360User);
         HalResource<License> halResource = createHalLicense(license);
 

rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/obligation/ObligationController.java
@@ -16,6 +16,11 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.sw360.datahandler.common.CommonUtils;
+import org.eclipse.sw360.datahandler.common.SW360Constants;
+import org.eclipse.sw360.datahandler.resourcelists.PaginationParameterException;
+import org.eclipse.sw360.datahandler.resourcelists.PaginationResult;
+import org.eclipse.sw360.datahandler.resourcelists.ResourceClassNotFoundException;
+import org.eclipse.sw360.datahandler.thrift.licenses.License;
 import org.eclipse.sw360.datahandler.thrift.licenses.Obligation;
 import org.eclipse.sw360.datahandler.thrift.RequestStatus;
 import org.eclipse.sw360.datahandler.thrift.users.User;
@@ -23,6 +28,7 @@ import org.eclipse.sw360.rest.resourceserver.core.HalResource;
 import org.eclipse.sw360.rest.resourceserver.core.MultiStatus;
 import org.eclipse.sw360.rest.resourceserver.core.RestControllerHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
 import org.springframework.hateoas.EntityModel;
@@ -35,7 +41,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -62,8 +70,8 @@ public class ObligationController implements RepresentationModelProcessor<Reposi
             tags = {"Obligations"}
     )
     @RequestMapping(value = OBLIGATION_URL, method = RequestMethod.GET)
-    public ResponseEntity<CollectionModel<EntityModel<Obligation>>> getObligations(
-            @RequestParam(value = "obligationLevel", required = false) String obligationLevel) {
+    public ResponseEntity<CollectionModel> getObligations(Pageable pageable, HttpServletRequest request,
+                                                          @RequestParam(value = "obligationLevel", required = false) String obligationLevel) throws ResourceClassNotFoundException, PaginationParameterException, URISyntaxException {
 
         List<Obligation> obligations;
         if (!CommonUtils.isNullEmptyOrWhitespace(obligationLevel)) {
@@ -74,13 +82,20 @@ public class ObligationController implements RepresentationModelProcessor<Reposi
             obligations = obligationService.getObligations();
         }
 
+        PaginationResult<Obligation> paginationResult = restControllerHelper.createPaginationResult(request, pageable, obligations, SW360Constants.TYPE_OBLIGATION);
         List<EntityModel<Obligation>> obligationResources = new ArrayList<>();
-        obligations.forEach(o -> {
-            Obligation embeddedObligation = restControllerHelper.convertToEmbeddedObligation(o);
-            obligationResources.add(EntityModel.of(embeddedObligation));
-        });
-
-        CollectionModel<EntityModel<Obligation>> resources = CollectionModel.of(obligationResources);
+        paginationResult.getResources().stream()
+                .forEach(obligation -> {
+                    Obligation embeddedObligation = restControllerHelper.convertToEmbeddedObligation(obligation);
+                    EntityModel<Obligation> licenseResource = EntityModel.of(embeddedObligation);
+                    obligationResources.add(licenseResource);
+                });
+        CollectionModel resources;
+        if (obligationResources.size() == 0) {
+            resources = restControllerHelper.emptyPageResource(License.class, paginationResult);
+        } else {
+            resources = restControllerHelper.generatePagesResource(paginationResult, obligationResources);
+        }
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 

rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/restdocs/ObligationSpecTest.java
@@ -43,6 +43,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ObligationSpecTest extends TestRestDocsSpecBase {
@@ -97,18 +99,31 @@ public class ObligationSpecTest extends TestRestDocsSpecBase {
     public void should_document_get_obligations() throws Exception {
         String accessToken = TestHelper.getAccessToken(mockMvc, testUserId, testUserPassword);
         mockMvc.perform(get("/api/obligations")
-                .header("Authorization", "Bearer " + accessToken)
-                .accept(MediaTypes.HAL_JSON))
+                        .header("Authorization", "Bearer " + accessToken)
+                        .param("page", "0")
+                        .param("page_entries", "5")
+                        .accept(MediaTypes.HAL_JSON))
                 .andExpect(status().isOk())
                 .andDo(this.documentationHandler.document(
+                        requestParameters(
+                                parameterWithName("page").description("Page of licenses"),
+                                parameterWithName("page_entries").description("Amount of licenses per page")
+                        ),
                         links(
-                                linkWithRel("curies").description("Curies are used for online documentation")
+                                linkWithRel("curies").description("Curies are used for online documentation"),
+                                linkWithRel("first").description("Link to first page"),
+                                linkWithRel("last").description("Link to last page")
                         ),
                         responseFields(
                                 subsectionWithPath("_embedded.sw360:obligations[]title").description("The title of the obligation"),
                                 subsectionWithPath("_embedded.sw360:obligations[]obligationType").description("The obligationType of the obligation"),
                                 subsectionWithPath("_embedded.sw360:obligations").description("An array of <<resources-obligations, Obligations resources>>"),
-                                subsectionWithPath("_links").description("<<resources-index-links,Links>> to other resources")
+                                subsectionWithPath("_links").description("<<resources-index-links,Links>> to other resources"),
+                                fieldWithPath("page").description("Additional paging information"),
+                                fieldWithPath("page.size").description("Number of licenses per page"),
+                                fieldWithPath("page.totalElements").description("Total number of all existing obligations"),
+                                fieldWithPath("page.totalPages").description("Total number of pages"),
+                                fieldWithPath("page.number").description("Number of the current page")
                         )));
     }
 

eclipse-sw360/sw360

Signed-off-by: hoangnt2 <hoang2.nguyenthai@toshiba.co.jp>

rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/license/LicenseController.java
@@ -22,6 +22,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
 import org.eclipse.sw360.datahandler.common.CommonUtils;
+import org.eclipse.sw360.datahandler.common.SW360Constants;
+import org.eclipse.sw360.datahandler.resourcelists.PaginationParameterException;
+import org.eclipse.sw360.datahandler.resourcelists.PaginationResult;
+import org.eclipse.sw360.datahandler.resourcelists.ResourceClassNotFoundException;
 import org.eclipse.sw360.datahandler.thrift.RequestStatus;
 import org.eclipse.sw360.datahandler.thrift.RequestSummary;
 import org.eclipse.sw360.datahandler.thrift.licenses.License;
@@ -31,6 +35,7 @@ import org.eclipse.sw360.datahandler.thrift.users.User;
 import org.eclipse.sw360.rest.resourceserver.core.HalResource;
 import org.eclipse.sw360.rest.resourceserver.core.RestControllerHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
 import org.springframework.hateoas.CollectionModel;
@@ -52,6 +57,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
 
+import java.net.URISyntaxException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -65,6 +71,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @SecurityRequirement(name = "basic")
 public class LicenseController implements RepresentationModelProcessor<RepositoryLinksResource> {
     public static final String LICENSES_URL = "/licenses";
+    public static final String LICENSE_TYPES_URL = "/licenseTypes";
 
     @NonNull
     private final Sw360LicenseService licenseService;
@@ -81,17 +88,22 @@ public class LicenseController implements RepresentationModelProcessor<Repositor
             tags = {"Licenses"}
     )
     @RequestMapping(value = LICENSES_URL, method = RequestMethod.GET)
-    public ResponseEntity<CollectionModel<EntityModel<License>>> getLicenses() throws TException {
+    public ResponseEntity<CollectionModel> getLicenses(Pageable pageable, HttpServletRequest request) throws TException, ResourceClassNotFoundException, PaginationParameterException, URISyntaxException {
         List<License> sw360Licenses = licenseService.getLicenses();
-
+        PaginationResult<License> paginationResult = restControllerHelper.createPaginationResult(request, pageable, sw360Licenses, SW360Constants.TYPE_LICENSE);
         List<EntityModel<License>> licenseResources = new ArrayList<>();
-        for (License sw360License : sw360Licenses) {
-            License embeddedLicense = restControllerHelper.convertToEmbeddedLicense(sw360License);
-            EntityModel<License> licenseResource = EntityModel.of(embeddedLicense);
-            licenseResources.add(licenseResource);
+        paginationResult.getResources().stream()
+                .forEach(license -> {
+                    License embeddedLicense = restControllerHelper.convertToEmbeddedLicense(license);
+                    EntityModel<License> licenseResource = EntityModel.of(embeddedLicense);
+                    licenseResources.add(licenseResource);
+                });
+        CollectionModel resources;
+        if (licenseResources.size() == 0) {
+            resources = restControllerHelper.emptyPageResource(License.class, paginationResult);
+        } else {
+            resources = restControllerHelper.generatePagesResource(paginationResult, licenseResources);
         }
-
-        CollectionModel<EntityModel<License>> resources = CollectionModel.of(licenseResources);
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
@@ -105,6 +117,20 @@ public class LicenseController implements RepresentationModelProcessor<Repositor
             obligationResources.add(EntityModel.of(embeddedObligation));
         });
         CollectionModel<EntityModel<Obligation>> resources = CollectionModel.of(obligationResources);
+        return new ResponseEntity<>(resources, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = LICENSE_TYPES_URL, method = RequestMethod.GET)
+    public ResponseEntity<CollectionModel<EntityModel<LicenseType>>> getLicenseTypes() throws TException {
+        List<LicenseType> sw360LicenseTypes = licenseService.getLicenseTypes();
+
+        List<EntityModel<LicenseType>> licenseTypeResources = new ArrayList<>();
+        for (LicenseType sw360LicenseType : sw360LicenseTypes) {
+            LicenseType embeddedLicenseType = restControllerHelper.convertToEmbeddedLicenseType(sw360LicenseType);
+            EntityModel<LicenseType> licenseResource = EntityModel.of(embeddedLicenseType);
+            licenseTypeResources.add(licenseResource);
+        }
+        CollectionModel<EntityModel<LicenseType>> resources = CollectionModel.of(licenseTypeResources);
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 
@@ -151,6 +177,10 @@ public class LicenseController implements RepresentationModelProcessor<Repositor
             @RequestBody License license
     ) throws TException {
         User sw360User = restControllerHelper.getSw360UserFromAuthentication();
+        List<License> sw360Licenses = licenseService.getLicenses();
+        if(restControllerHelper.checkDuplicateLicense(sw360Licenses, license.shortname)) {
+            return new ResponseEntity("sw360 license with name " + license.shortname + " already exists.", HttpStatus.CONFLICT);
+        }
         license = licenseService.createLicense(license, sw360User);
         HalResource<License> halResource = createHalLicense(license);
 

rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/obligation/ObligationController.java
@@ -12,10 +12,16 @@ package org.eclipse.sw360.rest.resourceserver.obligation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.sw360.datahandler.common.CommonUtils;
+import org.eclipse.sw360.datahandler.common.SW360Constants;
+import org.eclipse.sw360.datahandler.resourcelists.PaginationParameterException;
+import org.eclipse.sw360.datahandler.resourcelists.PaginationResult;
+import org.eclipse.sw360.datahandler.resourcelists.ResourceClassNotFoundException;
+import org.eclipse.sw360.datahandler.thrift.licenses.License;
 import org.eclipse.sw360.datahandler.thrift.licenses.Obligation;
 import org.eclipse.sw360.datahandler.thrift.RequestStatus;
 import org.eclipse.sw360.datahandler.thrift.users.User;
@@ -23,6 +29,7 @@ import org.eclipse.sw360.rest.resourceserver.core.HalResource;
 import org.eclipse.sw360.rest.resourceserver.core.MultiStatus;
 import org.eclipse.sw360.rest.resourceserver.core.RestControllerHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
 import org.springframework.hateoas.EntityModel;
@@ -36,6 +43,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -63,8 +71,8 @@ public class ObligationController implements RepresentationModelProcessor<Reposi
             tags = {"Obligations"}
     )
     @RequestMapping(value = OBLIGATION_URL, method = RequestMethod.GET)
-    public ResponseEntity<CollectionModel<EntityModel<Obligation>>> getObligations(
-            @RequestParam(value = "obligationLevel", required = false) String obligationLevel) {
+    public ResponseEntity<CollectionModel> getObligations(Pageable pageable, HttpServletRequest request,
+                                                          @RequestParam(value = "obligationLevel", required = false) String obligationLevel) throws ResourceClassNotFoundException, PaginationParameterException, URISyntaxException {
 
         List<Obligation> obligations;
         if (!CommonUtils.isNullEmptyOrWhitespace(obligationLevel)) {
@@ -75,13 +83,20 @@ public class ObligationController implements RepresentationModelProcessor<Reposi
             obligations = obligationService.getObligations();
         }
 
+        PaginationResult<Obligation> paginationResult = restControllerHelper.createPaginationResult(request, pageable, obligations, SW360Constants.TYPE_OBLIGATION);
         List<EntityModel<Obligation>> obligationResources = new ArrayList<>();
-        obligations.forEach(o -> {
-            Obligation embeddedObligation = restControllerHelper.convertToEmbeddedObligation(o);
-            obligationResources.add(EntityModel.of(embeddedObligation));
-        });
-
-        CollectionModel<EntityModel<Obligation>> resources = CollectionModel.of(obligationResources);
+        paginationResult.getResources().stream()
+                .forEach(obligation -> {
+                    Obligation embeddedObligation = restControllerHelper.convertToEmbeddedObligation(obligation);
+                    EntityModel<Obligation> licenseResource = EntityModel.of(embeddedObligation);
+                    obligationResources.add(licenseResource);
+                });
+        CollectionModel resources;
+        if (obligationResources.size() == 0) {
+            resources = restControllerHelper.emptyPageResource(License.class, paginationResult);
+        } else {
+            resources = restControllerHelper.generatePagesResource(paginationResult, obligationResources);
+        }
         return new ResponseEntity<>(resources, HttpStatus.OK);
     }
 

rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/restdocs/ObligationSpecTest.java
@@ -38,11 +38,13 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ObligationSpecTest extends TestRestDocsSpecBase {
@@ -94,17 +96,30 @@ public class ObligationSpecTest extends TestRestDocsSpecBase {
     public void should_document_get_obligations() throws Exception {
         mockMvc.perform(get("/api/obligations")
                 .header("Authorization", TestHelper.generateAuthHeader(testUserId, testUserPassword))
+                .param("page", "0")
+                .param("page_entries", "5")
                 .accept(MediaTypes.HAL_JSON))
                 .andExpect(status().isOk())
                 .andDo(this.documentationHandler.document(
+                        queryParameters(
+                                parameterWithName("page").description("Page of obligations"),
+                                parameterWithName("page_entries").description("Amount of obligations per page")
+                        ),
                         links(
-                                linkWithRel("curies").description("Curies are used for online documentation")
+                                linkWithRel("curies").description("Curies are used for online documentation"),
+                                linkWithRel("first").description("Link to first page"),
+                                linkWithRel("last").description("Link to last page")
                         ),
                         responseFields(
                                 subsectionWithPath("_embedded.sw360:obligations[]title").description("The title of the obligation"),
                                 subsectionWithPath("_embedded.sw360:obligations[]obligationType").description("The obligationType of the obligation"),
                                 subsectionWithPath("_embedded.sw360:obligations").description("An array of <<resources-obligations, Obligations resources>>"),
-                                subsectionWithPath("_links").description("<<resources-index-links,Links>> to other resources")
+                                subsectionWithPath("_links").description("<<resources-index-links,Links>> to other resources"),
+                                fieldWithPath("page").description("Additional paging information"),
+                                fieldWithPath("page.size").description("Number of obligations per page"),
+                                fieldWithPath("page.totalElements").description("Total number of all existing obligations"),
+                                fieldWithPath("page.totalPages").description("Total number of pages"),
+                                fieldWithPath("page.number").description("Number of the current page")
                         )));
     }
 