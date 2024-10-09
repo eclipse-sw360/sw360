c35e05fbd02dd2c3342bee337ca0f3235564477b
Helio Chissini de Castro <heliocastro@gmail.com>
2024-10-09 20:43:22 +0200

    fix: Create sw360oauthclients database
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- libraries/datahandler/src/main/java/org/eclipse/sw360/datahandler/common/DatabaseSettings.java
+++ libraries/datahandler/src/main/java/org/eclipse/sw360/datahandler/common/DatabaseSettings.java
@@ -1,9 +1,8 @@
 /*
  * Copyright Siemens AG, 2014-2019,2024. Part of the SW360 Portal Project.
  *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
  */
@@ -36,6 +35,7 @@ public class DatabaseSettings {
     public static final String COUCH_DB_ATTACHMENTS;
     public static final String COUCH_DB_CHANGE_LOGS;
     public static final String COUCH_DB_CONFIG;
+    public static final String COUCH_DB_OAUTHCLIENTS;
     public static final String COUCH_DB_USERS;
     public static final String COUCH_DB_VM;
     public static final String COUCH_DB_SPDX;
@@ -53,10 +53,11 @@ public class DatabaseSettings {
         // Try ENV if set first
         COUCH_DB_URL = System.getenv("COUCHDB_URL") != null ? System.getenv("COUCHDB_URL")
                 : props.getProperty("couchdb.url", "http://localhost:5984");
-        COUCH_DB_USERNAME = Optional.ofNullable(System.getenv("COUCHDB_USER") != null ? System.getenv("COUCHDB_USER")
-                : props.getProperty("couchdb.user", ""));
-        COUCH_DB_PASSWORD = Optional
-                .ofNullable(System.getenv("COUCHDB_PASSWORD") != null ? System.getenv("COUCHDB_PASSWORD")
+        COUCH_DB_USERNAME = Optional
+                .ofNullable(System.getenv("COUCHDB_USER") != null ? System.getenv("COUCHDB_USER")
+                        : props.getProperty("couchdb.user", ""));
+        COUCH_DB_PASSWORD = Optional.ofNullable(
+                System.getenv("COUCHDB_PASSWORD") != null ? System.getenv("COUCHDB_PASSWORD")
                         : props.getProperty("couchdb.password", ""));
         COUCH_DB_DATABASE = props.getProperty("couchdb.database", "sw360db");
         COUCH_DB_ATTACHMENTS = props.getProperty("couchdb.attachments", "sw360attachments");
@@ -65,18 +66,19 @@ public class DatabaseSettings {
         COUCH_DB_USERS = props.getProperty("couchdb.usersdb", "sw360users");
         COUCH_DB_VM = props.getProperty("couchdb.vulnerability_management", "sw360vm");
         COUCH_DB_SPDX = props.getProperty("couchdb.sw360spdx", "sw360spdx");
+        COUCH_DB_OAUTHCLIENTS = props.getProperty("couchdb.sw360oauthclients", "sw360oauthclients");
         COUCH_DB_CACHE = Boolean.parseBoolean(props.getProperty("couchdb.cache", "true"));
 
         LUCENE_SEARCH_LIMIT = Integer.parseInt(props.getProperty("lucenesearch.limit", "25"));
-        LUCENE_LEADING_WILDCARD = Boolean.parseBoolean(props.getProperty("lucenesearch.leading.wildcard", "false"));
+        LUCENE_LEADING_WILDCARD =
+                Boolean.parseBoolean(props.getProperty("lucenesearch.leading.wildcard", "false"));
     }
 
     public static @NotNull Cloudant getConfiguredClient() {
         Cloudant client;
         if (COUCH_DB_USERNAME.isPresent() && COUCH_DB_PASSWORD.isPresent()) {
-            Authenticator authenticator = CouchDbSessionAuthenticator.newAuthenticator(
-                    COUCH_DB_USERNAME.get(),
-                    COUCH_DB_PASSWORD.get());
+            Authenticator authenticator = CouchDbSessionAuthenticator
+                    .newAuthenticator(COUCH_DB_USERNAME.get(), COUCH_DB_PASSWORD.get());
             client = new Cloudant("sw360-couchdb", authenticator);
         } else {
             client = Cloudant.newInstance("sw360-couchdb");
