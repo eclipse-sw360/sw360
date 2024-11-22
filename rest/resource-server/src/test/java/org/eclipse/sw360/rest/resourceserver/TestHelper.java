4918ecd8546731c8b70f92a7d488a185e3992a1a
Helio Chissini de Castro <heliocastro@gmail.com>
2024-11-22 18:06:10 +0100

    fix(test): Remove unused invalid entries
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

--- rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/TestHelper.java
+++ rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/TestHelper.java
@@ -1,6 +1,6 @@
 /*
- * Copyright Siemens AG, 2017. Part of the SW360 Portal Project. Copyright Bosch Software
- * Innovations GmbH, 2018.
+ * Copyright Siemens AG, 2017. Part of the SW360 Portal Project.
+ * Copyright Bosch Software Innovations GmbH, 2018.
  *
  * This program and the accompanying materials are made available under the terms of the Eclipse
  * Public License 2.0 which is available at https://www.eclipse.org/legal/epl-2.0/
