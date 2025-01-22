77394950df4139ffbd83a69b335645a8d541f82f
Keerthi B L <keerthi.bl@siemens.com>
2025-01-22 11:19:56 +0530

    fix(rest) : Add licenseInfoHeaderText in summaryAdministration api response

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/project/ProjectController.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/project/ProjectController.java
@@ -2949,6 +2949,9 @@ public class ProjectController implements RepresentationModelProcessor<Repositor
     ) throws TException {
         User sw360User = restControllerHelper.getSw360UserFromAuthentication();
         Project sw360Project = projectService.getProjectForUserById(id, sw360User);
+        if(sw360Project.getLicenseInfoHeaderText().isEmpty()){
+            sw360Project.setLicenseInfoHeaderText(projectService.getLicenseInfoHeaderText());
+        }
         Map<String, String> sortedExternalURLs = CommonUtils.getSortedMap(sw360Project.getExternalUrls(), true);
         sw360Project.setExternalUrls(sortedExternalURLs);
         sw360Project.setReleaseIdToUsage(null);
@@ -2961,7 +2964,6 @@ public class ProjectController implements RepresentationModelProcessor<Repositor
         sw360Project.unsetReleaseIdToUsage();
         sw360Project.unsetProjectResponsible();
         sw360Project.unsetSecurityResponsibles();
-        sw360Project.unsetLicenseInfoHeaderText();
 
         return new ResponseEntity<>(userHalResource, HttpStatus.OK);
     }
