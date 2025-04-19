3e5aae599a4acd62a8cf207fa308d7fd54750ad8
Gaurav Mishra <mishra.gaurav@siemens.com>
2025-04-19 17:44:00 +0530

    feat(cd): use gitlab mvn package
    Use gitlab mvn packages to transfer war between stages.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

---
+++ utils/internal/create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  PROJECT_ROOT="$(pwd)/../../"
+else
+  PROJECT_ROOT="$1"
+fi
+
+printf '#!/bin/bash\n'
+
+find $PROJECT_ROOT -name "pom.xml" -exec sh -c '
+    if grep -q "<packaging>war</packaging>" {}; then
+        groupId=$(mvn -B -f {} help:evaluate -Dexpression=project.groupId -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
+        artifactId=$(mvn -B -f {} help:evaluate -Dexpression=project.artifactId -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
+        version=$(mvn -B -f {} help:evaluate -Dexpression=project.version -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
+        echo "mvn -q --no-transfer-progress dependency:get -Dartifact=$groupId:$artifactId:$version:war -Dtransitive=false -DremoteRepositories=gitlab-maven::::https://code.siemens.com/api/v4/projects/708/packages/maven"
+    fi
+' \;
