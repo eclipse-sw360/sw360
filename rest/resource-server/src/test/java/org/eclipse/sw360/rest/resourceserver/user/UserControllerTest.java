bea1742760ea2a1e881dc447832721554a63be04
Rajnish Kumar <22IT3036@rgipt.ac.in>
2025-04-19 16:58:47 +0530

    feat(rest):  Added License Header

--- rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/user/UserControllerTest.java
+++ rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/user/UserControllerTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Your Organization
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+
 package org.eclipse.sw360.rest.resourceserver.user;
 
 import org.eclipse.sw360.rest.resourceserver.user.model.UserDTO;
