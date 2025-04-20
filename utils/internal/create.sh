37f7371e89469ad3a727454492139c47ef34ea63
Gaurav Mishra <mishra.gaurav@siemens.com>
2025-04-20 20:32:57 +0530

    fix(cd): fix docker paths
    1. Fix the copy path in sw360 image as ENV is not carried forward.
    2. Optimize the create.sh to single command instead of 3.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- utils/internal/create.sh
+++ utils/internal/create.sh
@@ -8,20 +8,9 @@ fi
 
 printf '#!/bin/bash\n'
 
-find $PROJECT_ROOT/backend -name "pom.xml" -exec sh -c '
+find $PROJECT_ROOT/backend $PROJECT_ROOT/rest -name "pom.xml" -exec bash -c '
     if grep -q "<packaging>war</packaging>" {}; then
-        groupId=$(mvn -B -f {} help:evaluate -Dexpression=project.groupId -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
-        artifactId=$(mvn -B -f {} help:evaluate -Dexpression=project.artifactId -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
-        version=$(mvn -B -f {} help:evaluate -Dexpression=project.version -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
-        echo "mvn --no-transfer-progress dependency:get -Dartifact=$groupId:$artifactId:$version:war -Dtransitive=false -DremoteRepositories=gitlab-maven::::https://code.siemens.com/api/v4/projects/708/packages/maven"
-    fi
-' \;
-
-find $PROJECT_ROOT/rest -name "pom.xml" -exec sh -c '
-    if grep -q "<packaging>war</packaging>" {}; then
-        groupId=$(mvn -B -f {} help:evaluate -Dexpression=project.groupId -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
-        artifactId=$(mvn -B -f {} help:evaluate -Dexpression=project.artifactId -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
-        version=$(mvn -B -f {} help:evaluate -Dexpression=project.version -q -DforceStdout -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
-        echo "mvn --no-transfer-progress dependency:get -Dartifact=$groupId:$artifactId:$version:war -Dtransitive=false -DremoteRepositories=gitlab-maven::::https://code.siemens.com/api/v4/projects/708/packages/maven"
+        artifact=$(echo "\${project.groupId}:\${project.artifactId}:\${project.version}:war" | mvn -N -q -DforceStdout help:evaluate -f {} -Dstyle.color=never 2>/dev/null | sed -r "s/\x1B\[[0-9;]*[mK]//g")
+        echo "mvn --no-transfer-progress dependency:get -Dartifact=$artifact -Dtransitive=false -DremoteRepositories=gitlab-maven::::https://code.siemens.com/api/v4/projects/708/packages/maven"
     fi
 ' \;
