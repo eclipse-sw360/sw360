299a95c1b19e195f72a7a9ae91303312633d3cb5
Gaurav Mishra <mishra.gaurav@siemens.com>
2025-04-21 15:39:19 +0530

    fix(cd): export variables for envsubst
    envsubst only read env vars, so export them. Also fix path for catalina.sh
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- scripts/docker-config/docker-entrypoint.sh
+++ scripts/docker-config/docker-entrypoint.sh
@@ -4,35 +4,35 @@
 set -o errexit -o nounset -o pipefail
 
 # Set default values for environment variables
-COUCHDB_URL="${COUCHDB_URL:-http://couchdb:5984}"
-COUCHDB_USER="${COUCHDB_USER:-admin}"
-COUCHDB_LUCENESEARCH_LIMIT="${COUCHDB_LUCENESEARCH_LIMIT:-1000}"
-ENABLE_DISKSPACE="${ENABLE_DISKSPACE:-false}"
-JWKS_ISSUER_URI="${JWKS_ISSUER_URI:-http://localhost:8080/authorization/oauth2/jwks}"
-JWKS_SET_URI="${JWKS_SET_URI:-http://localhost:8080/authorization/oauth2/jwks}"
-SVM_SW360_API_URL="${SVM_SW360_API_URL:-https://svmtest.cert.siemens.com}"
-JWKS_ISSUER="${JWKS_ISSUER:-http://localhost:8090}"
+export COUCHDB_URL="${COUCHDB_URL:-http://couchdb:5984}"
+export COUCHDB_USER="${COUCHDB_USER:-admin}"
+export COUCHDB_LUCENESEARCH_LIMIT="${COUCHDB_LUCENESEARCH_LIMIT:-1000}"
+export ENABLE_DISKSPACE="${ENABLE_DISKSPACE:-false}"
+export JWKS_ISSUER_URI="${JWKS_ISSUER_URI:-http://localhost:8080/authorization/oauth2/jwks}"
+export JWKS_SET_URI="${JWKS_SET_URI:-http://localhost:8080/authorization/oauth2/jwks}"
+export SVM_SW360_API_URL="${SVM_SW360_API_URL:-https://svmtest.cert.siemens.com}"
+export JWKS_ISSUER="${JWKS_ISSUER:-http://localhost:8090}"
 
 # Read secrets from Docker secrets if available
 if [ -f "/run/secrets/COUCHDB_PASSWORD" ]; then
-  COUCHDB_PASSWORD=$(cat /run/secrets/COUCHDB_PASSWORD)
+  export COUCHDB_PASSWORD=$(cat /run/secrets/COUCHDB_PASSWORD)
 else
-  COUCHDB_PASSWORD="${COUCHDB_PASSWORD:-admin}"
+  export COUCHDB_PASSWORD="${COUCHDB_PASSWORD:-admin}"
 fi
 if [ -f "/run/secrets/SVM_SW360_CERTIFICATE_PASSPHRASE" ]; then
-  SVM_SW360_CERTIFICATE_PASSPHRASE=$(cat /run/secrets/SVM_SW360_CERTIFICATE_PASSPHRASE)
+  export SVM_SW360_CERTIFICATE_PASSPHRASE=$(cat /run/secrets/SVM_SW360_CERTIFICATE_PASSPHRASE)
 else
-  SVM_SW360_CERTIFICATE_PASSPHRASE="${SVM_SW360_CERTIFICATE_PASSPHRASE:-}"
+  export SVM_SW360_CERTIFICATE_PASSPHRASE="${SVM_SW360_CERTIFICATE_PASSPHRASE}"
 fi
 if [ -f "/run/secrets/SVM_SW360_JKS_PASSWORD" ]; then
-  SVM_SW360_JKS_PASSWORD=$(cat /run/secrets/SVM_SW360_JKS_PASSWORD)
+  export SVM_SW360_JKS_PASSWORD=$(cat /run/secrets/SVM_SW360_JKS_PASSWORD)
 else
-  SVM_SW360_JKS_PASSWORD="${SVM_SW360_JKS_PASSWORD:-}"
+  export SVM_SW360_JKS_PASSWORD="${SVM_SW360_JKS_PASSWORD}"
 fi
 if [ -f "/run/secrets/REST_APITOKEN_HASH_SALT" ]; then
-  REST_APITOKEN_HASH_SALT=$(cat /run/secrets/REST_APITOKEN_HASH_SALT)
+  export REST_APITOKEN_HASH_SALT=$(cat /run/secrets/REST_APITOKEN_HASH_SALT)
 else
-  REST_APITOKEN_HASH_SALT="${REST_APITOKEN_HASH_SALT:-}"
+  export REST_APITOKEN_HASH_SALT="${REST_APITOKEN_HASH_SALT}"
 fi
 
 mkdir -p /etc/sw360/authorization /etc/sw360/rest
@@ -57,4 +57,4 @@ done
 echo
 echo 'SW360 configuration complete; Starting up...'
 echo
-$CATALINA_HOME/catalina.sh run
+$CATALINA_HOME/bin/catalina.sh run
