6f6eb2021cbd2df3d94e179d633a98dea8bd6ba1
Achal Jhawar <35405812+achaljhawar@users.noreply.github.com>
2025-05-14 17:25:11 +0530

    Add proper self-link with project ID in licenseClearing endpoint (#3135)
    * fix(rest): Set project ID in licenseClearing response to prevent duplicate self-links
    
    Signed-off-by: Achal Jhawar <35405812+achaljhawar@users.noreply.github.com>
    Co-authored-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/project/ProjectController.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/project/ProjectController.java
@@ -2627,12 +2627,12 @@ public class ProjectController implements RepresentationModelProcessor<Repositor
 		Map<String, ProjectReleaseRelationship> releaseIdToUsage = sw360Project.getReleaseIdToUsage();
 		sw360.setReleaseIdToUsage(sw360Project.getReleaseIdToUsage());
 		sw360.setLinkedProjects(sw360Project.getLinkedProjects());
+		sw360.setId(sw360Project.getId());
 		sw360.unsetState();
 		sw360.unsetProjectType();
 		sw360.unsetVisbility();
 		sw360.unsetSecurityResponsibles();
 		HalResource<Project> halProject = new HalResource<>(sw360);
-
 		if (releaseIdToUsage != null) {
 		    restControllerHelper.addEmbeddedProjectReleases(halProject, releases);
 		}
