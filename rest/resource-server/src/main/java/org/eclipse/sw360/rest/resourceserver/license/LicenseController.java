e4042b209c61fb97b5f7fceb693ffa43c3dc34e1
Nikesh kumar <kumar.nikesh@siemens.com>
2025-06-10 10:12:44 +0530

    fix(rest): add license type usage check and restructure delete API response
    Signed-off-by: Nikesh kumar <kumar.nikesh@siemens.com>

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/license/LicenseController.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/license/LicenseController.java
@@ -47,6 +47,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
@@ -54,6 +55,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatus.Series;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
@@ -562,6 +564,29 @@ public class LicenseController implements RepresentationModelProcessor<Repositor
         User sw360User = restControllerHelper.getSw360UserFromAuthentication();
         RequestStatus status = licenseService.deleteLicenseType(id, sw360User);
 
-        return ResponseEntity.ok("License type deleted successfully: " + status.name());
+        Map<String, String> response = new HashMap<>();
+        response.put("status", status.name());
+
+        switch (status) {
+        case SUCCESS:
+            Map<String, String> successResponse = new HashMap<>();
+            successResponse.put("status", status.name());
+            successResponse.put("message", "License type deleted successfully.");
+            return ResponseEntity.ok(successResponse);
+
+        case IN_USE:
+            throw new HttpClientErrorException(HttpStatus.CONFLICT,
+                    "Cannot delete license type because it is currently in use.");
+
+        case ACCESS_DENIED:
+            throw new AccessDeniedException("User does not have permission to delete license type.");
+
+        case INVALID_INPUT:
+            throw new ResourceNotFoundException("License type with the given ID was not found.");
+
+        default:
+            throw new RuntimeException("Unexpected error occurred while deleting license type.");
+        }
     }
 }
+ 
