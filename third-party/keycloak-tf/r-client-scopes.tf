a17a92faf57e97c54153a78f7289586e575356fe
Gaurav Mishra <mishra.gaurav@siemens.com>
2025-06-24 11:23:26 +0530

    fix(kc): add user group to the token
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- third-party/keycloak-tf/r-client-scopes.tf
+++ third-party/keycloak-tf/r-client-scopes.tf
@@ -17,7 +17,26 @@ resource "keycloak_openid_client_scope" "sw360_write" {
   include_in_token_scope = true
 }
 
-# 3. Bind the scopes to SW360 UI realm. arc, basic, profile, email, roles &
+# 3. userGroup scope
+resource "keycloak_openid_client_scope" "user_group" {
+  realm_id               = keycloak_realm.sw360.id
+  name                   = "userGroup"
+  description            = "Get User Groups"
+  include_in_token_scope = true
+}
+
+# 3.1. Add mapper for SW360 UI client
+resource "keycloak_openid_group_membership_protocol_mapper" "user_group" {
+  realm_id            = keycloak_realm.sw360.id
+  claim_name          = "userGroup"
+  name                = "userGroup"
+  client_scope_id     = keycloak_openid_client_scope.user_group.id
+  add_to_access_token = true
+  add_to_id_token     = true
+  add_to_userinfo     = true
+}
+
+# 4. Bind the scopes to SW360 UI realm. arc, basic, profile, email, roles &
 # web-origins are used by KC by-default
 resource "keycloak_openid_client_default_scopes" "client_read" {
   realm_id  = keycloak_realm.sw360.id
@@ -31,10 +50,11 @@ resource "keycloak_openid_client_default_scopes" "client_read" {
     "web-origins",
     keycloak_openid_client_scope.sw360_read.name,
     keycloak_openid_client_scope.sw360_write.name,
+    keycloak_openid_client_scope.user_group.name,
   ]
 }
 
-# 4. Assign scopes for grafana client
+# 5. Assign scopes for grafana client
 resource "keycloak_openid_client_default_scopes" "grafana" {
   realm_id  = keycloak_realm.sw360.id
   client_id = keycloak_openid_client.grafana.id
@@ -45,7 +65,7 @@ resource "keycloak_openid_client_default_scopes" "grafana" {
   ]
 }
 
-# 4.1. Add group mapper for grafana client
+# 5.1. Add group mapper for grafana client
 resource "keycloak_openid_group_membership_protocol_mapper" "grafana_group_mapper" {
   realm_id   = keycloak_realm.sw360.id
   client_id  = keycloak_openid_client.grafana.id
