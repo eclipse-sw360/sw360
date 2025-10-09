8e09f74f32a2b9db2c552d04b10aa817d58c8548
Bibhuti Bhusan Dash <bibhuti230185@gmail.com>
2025-10-09 14:52:43 +0530

    feat(SVM): Optimize the sync between SW360 and SVM

--- backend/vmcomponents/src/main/java/org/eclipse/sw360/vmcomponents/common/SVMUtils.java
+++ backend/vmcomponents/src/main/java/org/eclipse/sw360/vmcomponents/common/SVMUtils.java
@@ -8,6 +8,7 @@ import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.sw360.datahandler.common.CommonUtils;
 import org.eclipse.sw360.datahandler.db.SvmHttpClientFactory;
 import org.eclipse.sw360.datahandler.thrift.RequestStatus;
 import org.eclipse.sw360.datahandler.thrift.RequestSummary;
@@ -28,17 +29,28 @@ public class SVMUtils {
 
     private SVMUtils() {}
 
+    public static String prepareJSONRequestAndGetResponse(String url) throws IOException {
+        return prepareJSONRequestAndGetResponse(url, null);
+    }
+
     /**
      * Performs an HTTP GET request against the given URL and returns the response body as a String.
+     * If {@code modifiedAfter} provided, use it with {@code modified_after} parameter.
      *
      * @param url the URL to call
+     * @param modifiedAfter timestamp to fetch updates after
      * @return the response body
      * @throws IOException if the request fails or returns a non-200 status code
      */
-    public static String prepareJSONRequestAndGetResponse(String url) throws IOException {
-        log.debug("Call URL: {}", url);
+    public static String prepareJSONRequestAndGetResponse(String url, String modifiedAfter) throws IOException {
+        String finalUrl = url;
+        if (CommonUtils.isNotNullEmptyOrWhitespace(modifiedAfter)) {
+            String separator = url.contains("?") ? "&" : "?";
+            finalUrl = url + separator + "modified_after=" + modifiedAfter;
+        }
+        log.debug("Call URL: {}", finalUrl);
         CloseableHttpClient httpClient = SvmHttpClientFactory.getTrustedClient();
-        HttpGet httpGet = new HttpGet(url);
+        HttpGet httpGet = new HttpGet(finalUrl);
         httpGet.setHeader("Accept", "application/json");
         return httpClient.execute(httpGet, response -> {
             int code = response.getCode();
@@ -51,6 +63,25 @@ public class SVMUtils {
             log.debug("Response from Server .... \n{}", body);
             return body;
         });
+    }
+
+    public static String calculateModifiedAfter(String lastEndDate, int offsetDays) {
+        try {
+            if (lastEndDate == null || lastEndDate.isEmpty()) {
+                return null;
+            }
+            SimpleDateFormat sw360Format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date lastDate = sw360Format.parse(lastEndDate);
+
+            LocalDateTime lastDateTime = LocalDateTime.ofInstant(lastDate.toInstant(), java.time.ZoneId.systemDefault());
+            LocalDateTime modifiedAfterDateTime = lastDateTime.minus(offsetDays, ChronoUnit.DAYS);
+
+            DateTimeFormatter svmFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+            return modifiedAfterDateTime.format(svmFormat);
+        } catch (Exception e) {
+            log.error("Error calculating modified_after parameter: " + e.getMessage(), e);
+            return null;
+        }
     }
 
     public static RequestSummary newRequestSummary(RequestStatus status, int totalElements, int totalAffectedElements, String message) {
