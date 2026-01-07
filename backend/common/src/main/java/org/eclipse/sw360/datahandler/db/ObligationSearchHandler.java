edd5234bf7044c94f7ddc9c65b456a1b593236b3
Gaurav Mishra <mishra.gaurav@siemens.com>
2026-01-07 15:14:03 +0530

    fix(obligation): fix the pagination /obligations
    Fix the pagination and search for the `/obligations` endpoint.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- backend/common/src/main/java/org/eclipse/sw360/datahandler/db/ObligationSearchHandler.java
+++ backend/common/src/main/java/org/eclipse/sw360/datahandler/db/ObligationSearchHandler.java
@@ -10,18 +10,28 @@
  */
 package org.eclipse.sw360.datahandler.db;
 
+import com.google.common.base.Joiner;
 import com.ibm.cloud.cloudant.v1.Cloudant;
 import com.google.gson.Gson;
 import org.eclipse.sw360.datahandler.cloudantclient.DatabaseConnectorCloudant;
+import org.eclipse.sw360.datahandler.common.CommonUtils;
 import org.eclipse.sw360.datahandler.common.DatabaseSettings;
 import org.eclipse.sw360.datahandler.couchdb.lucene.NouveauLuceneAwareDatabaseConnector;
+import org.eclipse.sw360.datahandler.thrift.PaginationData;
 import org.eclipse.sw360.datahandler.thrift.licenses.Obligation;
+import org.eclipse.sw360.datahandler.thrift.licenses.ObligationLevel;
+import org.eclipse.sw360.datahandler.thrift.licenses.ObligationSortColumn;
 import org.eclipse.sw360.nouveau.designdocument.NouveauDesignDocument;
 import org.eclipse.sw360.nouveau.designdocument.NouveauIndexDesignDocument;
 import org.eclipse.sw360.nouveau.designdocument.NouveauIndexFunction;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.eclipse.sw360.datahandler.couchdb.lucene.NouveauLuceneAwareDatabaseConnector.prepareWildcardQuery;
 import static org.eclipse.sw360.nouveau.LuceneAwareCouchDbConnector.DEFAULT_DESIGN_PREFIX;
@@ -33,15 +43,24 @@ public class ObligationSearchHandler {
     private static final NouveauIndexDesignDocument luceneSearchView
         = new NouveauIndexDesignDocument("obligations",
             new NouveauIndexFunction(
-                "function(doc) {" +
-                "    if(!doc.type || doc.type != 'obligation') return;" +
-                "    if(doc.title !== undefined && doc.title != null && doc.title.length >0) {" +
-                "      index('text', 'title', doc.title, {'store': true});" +
-                "    }" +
-                "    if(doc.text !== undefined && doc.text != null && doc.text.length >0) {" +
-                "      index('text', 'text', doc.text, {'store': true});" +
-                "    }" +
-                "}"));
+                """
+                function(doc) {
+                    if(!doc.type || doc.type != 'obligation') return;
+                    if(doc.title !== undefined && doc.title != null && doc.title.length >0) {
+                      index('text', 'title', doc.title, {'store': true});
+                      index('string', 'title_sort', doc.title);
+                    }
+                    if(doc.text !== undefined && doc.text != null && doc.text.length >0) {
+                      index('text', 'text', doc.text, {'store': true});
+                      index('string', 'text_sort', doc.text);
+                    }
+                    if(doc.obligationLevel !== undefined && doc.obligationLevel != null && doc.obligationLevel.length >0) {
+                      index('text', 'obligationLevel', doc.obligationLevel, {'store': true});
+                      index('string', 'obligationLevel_sort', doc.obligationLevel);
+                    }
+                }
+                """
+                ));
 
     private final NouveauLuceneAwareDatabaseConnector connector;
 
@@ -60,5 +79,77 @@ public class ObligationSearchHandler {
     public List<Obligation> search(String searchText) {
         return connector.searchView(Obligation.class, luceneSearchView.getIndexName(),
                 prepareWildcardQuery(searchText));
+    }
+
+    /**
+     * Search for Obligations in paginated manner based on searchText in title
+     * or text or obligationLevel.
+     * @param searchText      Text to search
+     * @param obligationLevel Obligation level to filter
+     * @param pageData        Pagination data
+     * @return Paginated filtered obligations list.
+     */
+    public Map<PaginationData, List<Obligation>> searchWithPagination(
+            String searchText, ObligationLevel obligationLevel, PaginationData pageData
+    ) {
+        String sortColumn = getSortColumnName(pageData);
+
+        Map<String, Map<String, Set<String>>> restrictions = new HashMap<>();
+
+        if (CommonUtils.isNotNullEmptyOrWhitespace(searchText)) {
+            Map<String, Set<String>> textFilter = new HashMap<>();
+
+            String queryString = prepareWildcardQuery(searchText);
+            textFilter.put(
+                    Obligation._Fields.TITLE.getFieldName(),
+                    Collections.singleton(queryString));
+            textFilter.put(
+                    Obligation._Fields.TEXT.getFieldName(),
+                    Collections.singleton(queryString));
+
+            restrictions.put("OR", textFilter);
+        }
+
+        if (obligationLevel != null) {
+            Map<String, Set<String>> levelFilter = new HashMap<>();
+            levelFilter.put(
+                    Obligation._Fields.OBLIGATION_LEVEL.getFieldName(),
+                    Collections.singleton(obligationLevel.toString())
+            );
+
+            restrictions.put("AND", levelFilter);
+        }
+
+        List<String> queryFilters = NouveauLuceneAwareDatabaseConnector.createComplexQuery(
+                Obligation.class, null, restrictions
+        );
+
+        final Joiner AND = Joiner.on(" AND ");
+
+        String finalQuery = AND.join(queryFilters);
+
+        Map<PaginationData, List<Obligation>> resultObligationList = connector
+                .searchView(Obligation.class,
+                        luceneSearchView.getIndexName(), finalQuery,
+                        pageData, sortColumn, pageData.isAscending());
+
+        PaginationData respPageData = resultObligationList.keySet().iterator().next();
+        List<Obligation> obligationList = resultObligationList.values().iterator().next();
+
+        return Collections.singletonMap(respPageData, obligationList);
+    }
+
+    /**
+     * Convert sort column number back to sorting column name. This function makes sure to use the string column (with
+     * `_sort` suffix) for text indexes.
+     * @param pageData Pagination Data from the request.
+     * @return Sort column name. Defaults to title
+     */
+    private static @Nonnull String getSortColumnName(@Nonnull PaginationData pageData) {
+        return switch (ObligationSortColumn.findByValue(pageData.getSortColumnNumber())) {
+            case ObligationSortColumn.BY_TEXT -> "text_sort";
+            case ObligationSortColumn.BY_LEVEL -> "obligationLevel_sort";
+            case null, default -> "title_sort";
+        };
     }
 }
--- backend/licenses-core/src/main/java/org/eclipse/sw360/licenses/db/TodoRepository.java
+++ backend/licenses-core/src/main/java/org/eclipse/sw360/licenses/db/TodoRepository.java
@@ -9,14 +9,19 @@
  */
 package org.eclipse.sw360.licenses.db;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.sw360.datahandler.cloudantclient.DatabaseConnectorCloudant;
 import org.eclipse.sw360.datahandler.cloudantclient.DatabaseRepositoryCloudantClient;
+import org.eclipse.sw360.datahandler.thrift.PaginationData;
 import org.eclipse.sw360.datahandler.thrift.licenses.Obligation;
 
 import com.ibm.cloud.cloudant.v1.model.DesignDocumentViewsMapReduce;
+import org.eclipse.sw360.datahandler.thrift.licenses.ObligationSortColumn;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * CRUD access for the Obligation class
@@ -28,11 +33,50 @@ public class TodoRepository extends DatabaseRepositoryCloudantClient<Obligation>
 
     private static final String ALL = "function(doc) { if (doc.type == 'obligation') emit(null, doc._id) }";
 
+    private static final String BY_LOWERCASE_OBLIGATION_TITLE_VIEW =
+            "function(doc) {" +
+                    "  if (doc.type == 'obligation' && doc.title != null) {" +
+                    "    emit(doc.title.toLowerCase(), doc._id);" +
+                    "  } " +
+                    "}";
+
+    private static final String BY_LOWERCASE_OBLIGATION_TEXT_VIEW =
+            "function(doc) {" +
+                    "  if (doc.type == 'obligation' && doc.text != null) {" +
+                    "    emit(doc.text.toLowerCase(), doc._id);" +
+                    "  } " +
+                    "}";
+
+    private static final String BY_LOWERCASE_OBLIGATION_LEVEL_VIEW =
+            "function(doc) {" +
+                    "  if (doc.type == 'obligation' && doc.obligationLevel != null) {" +
+                    "    emit(doc.obligationLevel.toLowerCase(), doc._id);" +
+                    "  } " +
+                    "}";
+
     public TodoRepository(DatabaseConnectorCloudant db) {
         super(db, Obligation.class);
         Map<String, DesignDocumentViewsMapReduce> views = new HashMap<>();
         views.put("all", createMapReduce(ALL, null));
+        views.put("obligationbytitle", createMapReduce(BY_LOWERCASE_OBLIGATION_TITLE_VIEW, null));
+        views.put("obligationbytext", createMapReduce(BY_LOWERCASE_OBLIGATION_TEXT_VIEW, null));
+        views.put("obligationbylevel", createMapReduce(BY_LOWERCASE_OBLIGATION_LEVEL_VIEW, null));
         initStandardDesignDocument(views, db);
     }
 
+    public Map<PaginationData, List<Obligation>> getObligationsPaginated(PaginationData pageData) {
+        String viewName = getViewFromPagination(pageData);
+
+        List<Obligation> obligations = queryViewPaginated(viewName, pageData, false);
+
+        return Collections.singletonMap(pageData, obligations);
+    }
+
+    private static @NotNull String getViewFromPagination(PaginationData pageData) {
+        return switch (ObligationSortColumn.findByValue(pageData.getSortColumnNumber())) {
+            case ObligationSortColumn.BY_TEXT -> "obligationbytext";
+            case ObligationSortColumn.BY_LEVEL -> "obligationbylevel";
+            case null, default -> "obligationbytitle";
+        };
+    }
 }
