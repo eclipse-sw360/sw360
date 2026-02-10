b408799ce457cf427b18395419378e9fc6f352bb
Shivamrut <gshivamrut@gmail.com>
2026-02-10 15:56:28 +0530

    fix(rest): close streams in license upload/download
    Fixes #3678
    
    Signed-off-by: Shivamrut <gshivamrut@gmail.com>

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/license/Sw360LicenseService.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/license/Sw360LicenseService.java
@@ -244,13 +244,14 @@ public class Sw360LicenseService {
             String fileConstant="LicensesBackup.lics";
             Map<String, InputStream> fileNameToStreams = (new LicsExporter(sw360LicenseClient)).getFilenameToCSVStreams();
             final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            final ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream);
-
-            for (Map.Entry<String, InputStream> entry : fileNameToStreams.entrySet()) {
-                 ZipTools.addToZip(zipOutputStream, entry.getKey(), entry.getValue());
+            try (ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream)) {
+                for (Map.Entry<String, InputStream> entry : fileNameToStreams.entrySet()) {
+                    try (InputStream in = entry.getValue()) {
+                        ZipTools.addToZip(zipOutputStream, entry.getKey(), in);
+                    }
+                }
+                zipOutputStream.finish();
             }
-            zipOutputStream.flush();
-            zipOutputStream.close();
             final ByteArrayInputStream zipFile = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
             String filename = String.format(fileConstant, SW360Utils.getCreatedOn());
             response.setContentType(CONTENT_TYPE);
@@ -275,16 +276,26 @@ public class Sw360LicenseService {
         if (!PermissionUtils.isUserAtLeast(UserGroup.ADMIN, sw360User)) {
             throw new BadRequestClientException("Unable to upload license file. User is not admin");
         }
-        try {
-            InputStream inputStream = file.getInputStream();
+        try (InputStream inputStream = file.getInputStream()) {
             ZipTools.extractZipToInputStreamMap(inputStream, inputMap);
             LicenseService.Iface sw360LicenseClient = getThriftLicenseClient();
             final LicsImporter licsImporter = new LicsImporter(sw360LicenseClient, overwriteIfExternalIdMatches, overwriteIfIdMatchesEvenWithoutExternalIdMatch);
             licsImporter.importLics(sw360User, inputMap);
-
-        }finally {
-            for (InputStream inputStream : inputMap.values()) {
-                inputStream.close();
+        } finally {
+            IOException closeFailure = null;
+            for (InputStream in : inputMap.values()) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    if (closeFailure == null) {
+                        closeFailure = e;
+                    } else {
+                        closeFailure.addSuppressed(e);
+                    }
+                }
+            }
+            if (closeFailure != null) {
+                throw closeFailure;
             }
         }
 	}
