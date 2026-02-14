2c42e50336bdccc8e4a06ad9308a3c3a85523c33
Ali <aligadallah14@gmail.com>
2026-02-14 23:54:19 +0200

    fix(security): fix XXE vulnerability in XML parsing
    Signed-off-by: Ali <aligadallah14@gmail.com>

--- backend/licenseinfo/src/main/java/org/eclipse/sw360/licenseinfo/parsers/AbstractCLIParser.java
+++ backend/licenseinfo/src/main/java/org/eclipse/sw360/licenseinfo/parsers/AbstractCLIParser.java
@@ -140,6 +140,14 @@ public abstract class AbstractCLIParser extends LicenseInfoParser {
 
     protected Document getDocument(InputStream attachmentStream) throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        factory.setXIncludeAware(false);
+        factory.setExpandEntityReferences(false);
+
         DocumentBuilder builder = factory.newDocumentBuilder();
         return builder.parse(attachmentStream);
     }
