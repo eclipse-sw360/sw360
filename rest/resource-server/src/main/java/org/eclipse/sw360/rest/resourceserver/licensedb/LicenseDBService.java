ce501e61d451ffc4f03ba61c7d1181f76b4d7468
ADITYA-CODE-SOURCE <adityavishe67@gmail.com>
2026-03-10 09:17:33 +0530

    fix(licensedb): restore uploadLicense method and fix enum imports
    - Restore uploadLicense method in Sw360LicenseService.java
    - Add ObligationType and ObligationLevel imports to LicenseDBService.java
    - Fix enum references to use correct class names instead of nested types

--- rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/licensedb/LicenseDBService.java
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/licensedb/LicenseDBService.java
@@ -17,6 +17,8 @@ import org.eclipse.sw360.datahandler.common.SW360Constants;
 import org.eclipse.sw360.datahandler.thrift.licenses.License;
 import org.eclipse.sw360.datahandler.thrift.licenses.LicenseService;
 import org.eclipse.sw360.datahandler.thrift.licenses.Obligation;
+import org.eclipse.sw360.datahandler.thrift.licenses.ObligationLevel;
+import org.eclipse.sw360.datahandler.thrift.licenses.ObligationType;
 import org.eclipse.sw360.datahandler.thrift.users.User;
 import org.springframework.stereotype.Service;
 
@@ -207,7 +209,7 @@ public class LicenseDBService {
         if (obligationNode.has("obligation_type")) {
             String type = obligationNode.get("obligation_type").asText();
             try {
-                obligation.setObligationType(Obligation.ObligationType.valueOf(type.toUpperCase()));
+                obligation.setObligationType(ObligationType.valueOf(type.toUpperCase()));
             } catch (IllegalArgumentException e) {
                 log.warn("Unknown obligation type: {}", type);
             }
@@ -216,7 +218,7 @@ public class LicenseDBService {
         if (obligationNode.has("obligation_level")) {
             String level = obligationNode.get("obligation_level").asText();
             try {
-                obligation.setObligationLevel(Obligation.ObligationLevel.valueOf(level.toUpperCase()));
+                obligation.setObligationLevel(ObligationLevel.valueOf(level.toUpperCase()));
             } catch (IllegalArgumentException e) {
                 log.warn("Unknown obligation level: {}", level);
             }
