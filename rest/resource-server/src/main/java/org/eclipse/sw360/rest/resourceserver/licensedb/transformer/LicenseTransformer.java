1178268ae17778d2fd51fd9a275dfa3e19598831
ADITYA-CODE-SOURCE <adityavishe67@gmail.com>
2026-03-14 08:55:31 +0530

    feat(licensedb): Add data transformation layer for LicenseDB integration
    Added transformer classes to convert LicenseDB data to SW360 format:
    - LicenseTransformer: Maps LicenseDB License to SW360 License
    - ObligationTransformer: Maps LicenseDB Obligation to SW360 Obligation
    
    These transformers handle field mapping between external LicenseDB
    format and internal SW360 data model for licenses and obligations.
    
    Fixes #3897

---
+++ rest/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/licensedb/transformer/LicenseTransformer.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright Siemens AG, 2025. Part of the SW360 Portal Project.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.sw360.rest.resourceserver.licensedb.transformer;
+
+import org.eclipse.sw360.datahandler.thrift.licenses.License;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * Transformer class to convert LicenseDB license data to SW360 License format.
+ * 
+ * <p>This transformer handles the conversion of license data received from the
+ * LicenseDB service into the SW360 internal License data model. It maps all
+ * relevant fields and handles null values gracefully.</p>
+ * 
+ * <p>Field mapping:</p>
+ * <ul>
+ *   <li>shortname -> shortname</li>
+ *   <li>fullname -> fullname</li>
+ *   <li>text -> text</li>
+ *   <li>url -> url</li>
+ *   <li>licenseType -> licenseType</li>
+ *   <li>osiApproved -> osiApproved</li>
+ * </ul>
+ * 
+ * <p>Example usage:</p>
+ * <pre>
+ * LicenseDBLicense licenseDbData = licenseDBClient.getLicense("Apache-2.0");
+ * LicenseTransformer transformer = new LicenseTransformer();
+ * License sw360License = transformer.transform(licenseDbData);
+ * </pre>
+ */
+@Component
+public class LicenseTransformer {
+
+    /**
+     * Transform LicenseDB license data to SW360 License.
+     *
+     * @param licenseDbData the license data from LicenseDB as a Map
+     * @return SW360 License object, or null if input is null
+     */
+    public License transform(Map<String, Object> licenseDbData) {
+        if (licenseDbData == null) {
+            return null;
+        }
+        
+        License license = new License();
+        
+        // Map basic string fields
+        license.setShortname(getStringValue(licenseDbData, "shortname"));
+        license.setFullname(getStringValue(licenseDbData, "fullname"));
+        license.setText(getStringValue(licenseDbData, "text"));
+        license.setUrl(getStringValue(licenseDbData, "url"));
+        
+        // Map license type
+        String licenseType = getStringValue(licenseDbData, "licenseType");
+        if (licenseType != null) {
+            license.setLicenseType(licenseType);
+        }
+        
+        // Map OSI approved flag
+        Boolean osiApproved = getBooleanValue(licenseDbData, "osiApproved");
+        if (osiApproved != null) {
+            license.setOsiApproved(osiApproved);
+        }
+        
+        return license;
+    }
+    
+    /**
+     * Extract a string value from the data map.
+     *
+     * @param data the data map
+     * @param key the key to look up
+     * @return the string value, or null if not found
+     */
+    private String getStringValue(Map<String, Object> data, String key) {
+        Object value = data.get(key);
+        return value != null ? value.toString() : null;
+    }
+    
+    /**
+     * Extract a boolean value from the data map.
+     *
+     * @param data the data map
+     * @param key the key to look up
+     * @return the boolean value, or null if not found
+     */
+    private Boolean getBooleanValue(Map<String, Object> data, String key) {
+        Object value = data.get(key);
+        if (value == null) {
+            return null;
+        }
+        if (value instanceof Boolean) {
+            return (Boolean) value;
+        }
+        return Boolean.parseBoolean(value.toString());
+    }
+}
