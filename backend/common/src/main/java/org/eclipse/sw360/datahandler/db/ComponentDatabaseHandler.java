6d20f2d7d3b57afe232ac8c3a015a0dc8e3c9d7b
Uddeshya <srivastavauddeshya98@gmail.com>
2026-03-24 17:42:26 +0530

    refactor: replace printStackTrace with logger
    Improve null safety using Objects.equals to prevent NullPointerException in
    ComponentDatabaseHandler.

--- backend/common/src/main/java/org/eclipse/sw360/datahandler/db/ComponentDatabaseHandler.java
+++ backend/common/src/main/java/org/eclipse/sw360/datahandler/db/ComponentDatabaseHandler.java
@@ -1311,7 +1311,7 @@ public class ComponentDatabaseHandler extends AttachmentAwareDatabaseHandler {
             Set<String> updatedAttachmentId = updatedAttachments.stream().map(Attachment::getAttachmentContentId).collect(Collectors.toSet());
 
             // check if attachments are updated
-            if (!originalAttachmentId.equals(updatedAttachmentId)) {
+            if (!Objects.equals(originalAttachmentId, updatedAttachmentId)) {
                 // fetch all the projects associated with this release and collect the Clearing request Ids
                 final Set<Project> usingProjects = projectRepository.searchByReleaseId(release.getId());
                 final Set<String> crIds = CommonUtils.nullToEmptySet(usingProjects).stream()
