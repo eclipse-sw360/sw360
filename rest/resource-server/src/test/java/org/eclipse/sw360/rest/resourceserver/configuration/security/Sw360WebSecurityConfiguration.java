7086e08752be630b413f8c84c0e8d2645536b254
Shivamrut <gshivamrut@gmail.com>
2026-04-07 15:00:11 +0530

    chore(rest): removed unused imports from Sw360WebSecurityConfiguration
    Signed-off-by: Shivamrut <gshivamrut@gmail.com>

--- rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/configuration/security/Sw360WebSecurityConfiguration.java
+++ rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/configuration/security/Sw360WebSecurityConfiguration.java
@@ -13,12 +13,9 @@ package org.eclipse.sw360.rest.resourceserver.configuration.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Profile("SECURITY_MOCK")
