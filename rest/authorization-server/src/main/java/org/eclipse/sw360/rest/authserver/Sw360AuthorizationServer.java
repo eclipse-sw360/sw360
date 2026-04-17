ce3ba7f21ff6e326b3119de04dccfe04321eb9bc
Gaurav Mishra <mishra.gaurav@siemens.com>
2026-04-17 12:02:25 +0530

    fix(rest): harden headers and fix CORS origin
    Fixes weak content security policy vulnerabilities by implementing a
    strict Sw360SecurityFilter for the REST API.
    Resolves bugs in Sw360CORSFilter regarding comma-separated origin list
    parsing and improper literal string SpEL fallbacks.
    Updates Keycloak terraform CSP to close UI vulnerabilities and
    documents CORS overrides.
    
    Signed-off-by: Gaurav Mishra <mishra.gaurav@siemens.com>

--- rest/authorization-server/src/main/java/org/eclipse/sw360/rest/authserver/Sw360AuthorizationServer.java
+++ rest/authorization-server/src/main/java/org/eclipse/sw360/rest/authserver/Sw360AuthorizationServer.java
@@ -16,6 +16,7 @@ import org.eclipse.sw360.datahandler.common.CommonUtils;
 import org.eclipse.sw360.datahandler.thrift.users.UserGroup;
 import org.eclipse.sw360.rest.common.PropertyUtils;
 import org.eclipse.sw360.rest.common.Sw360CORSFilter;
+import org.eclipse.sw360.rest.common.Sw360SecurityFilter;
 import org.eclipse.sw360.rest.common.Sw360XssFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -23,7 +24,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({Sw360CORSFilter.class, Sw360XssFilter.class})
+@Import({Sw360CORSFilter.class, Sw360XssFilter.class, Sw360SecurityFilter.class})
 public class Sw360AuthorizationServer extends SpringBootServletInitializer {
 
     private static final String SW360_PROPERTIES_FILE_PATH = "/sw360.properties";
