f5d8e8df87bb6221f208b49e7ba8b6e4114c3e59
honeyverma1 <honeyverma8240@gmail.com>
2026-08-01 18:35:12 +0530

    refactor(tests): removed @Autowired annotations from import statements

--- rest/rest-common/src/main/java/org/eclipse/sw360/rest/common/security/authproviders/Sw360UserAuthenticationProvider.java
+++ rest/rest-common/src/main/java/org/eclipse/sw360/rest/common/security/authproviders/Sw360UserAuthenticationProvider.java
@@ -5,7 +5,6 @@ SPDX-License-Identifier: EPL-2.0
 package org.eclipse.sw360.rest.common.security.authproviders;
 
 import org.eclipse.sw360.rest.common.client.service.Sw360UserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
