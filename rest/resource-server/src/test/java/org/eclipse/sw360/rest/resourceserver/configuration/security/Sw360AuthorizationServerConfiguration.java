4eff9a7ba4463d4e18e25d7d19892fe30c6a0755
honeyverma1 <honeyverma8240@gmail.com>
2026-08-04 09:22:59 +0530

    refactor(Sw360AuthorizationServerConfiguration): added @Autowired annotations back

--- rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/configuration/security/Sw360AuthorizationServerConfiguration.java
+++ rest/resource-server/src/test/java/org/eclipse/sw360/rest/resourceserver/configuration/security/Sw360AuthorizationServerConfiguration.java
@@ -49,6 +49,7 @@ public class Sw360AuthorizationServerConfiguration {
 		return httpSecurity.build();
 	}
 
+	@Autowired
 	public void authenticationManagerBuilder(AuthenticationManagerBuilder authenticationManagerBuilder,
 			PasswordEncoder passwordEncoder, Sw360CustomUserDetailsService userDetailsService) {
 		authenticationManagerBuilder.authenticationProvider(
